44cfde3c03c12efdbdba7bb579e09b6055da7337
David Waller <david@krawaller.se>
2016-01-31 21:43:50 +0100

    renamed UNITS to UNITDATA in prep for layers split

--- dev/test/specs/effects.spec.js
+++ dev/test/specs/effects.spec.js
@@ -10,45 +10,45 @@ describe("The effect commands",()=>{
             scope: {
                 MARKS: {'1st':'a','2nd':'b'},
                 LAYERS: {units: {a:{id:'unit1',pos:'a'},b:{id:'unit2',pos:'b'}}},
-                UNITS: {unit1:{pos:'a'},unit2:{pos:'b'}}
+                UNITDATA: {unit1:{pos:'a'},unit2:{pos:'b'}}
             },
             mutations: {
-                UNITS: {unit1:{pos:'b'},unit2:{pos:'a'}}
+                UNITDATA: {unit1:{pos:'b'},unit2:{pos:'a'}}
             }
         }
     })
     test(E.killid,'the killid func',{
         'for straight call': {
             args: [42],
-            scope: { UNITS: { 42:{name:'foo'} } },
-            mutations: { UNITS: { 42: {name:'foo',dead:true} } }
+            scope: { UNITDATA: { 42:{name:'foo'} } },
+            mutations: { UNITDATA: { 42: {name:'foo',dead:true} } }
         }
     });
     test(E.kill1at,'the kill1at func',{
         'when noone there': {
             arg: ['mark','mymark'],
-            scope: { 'UNITS': {}, LAYERS: {units: {}}, 'MARKS': {mymark:'pos'}},
-            mutations: { 'UNITS': {} }
+            scope: { UNITDATA: {}, LAYERS: {units: {}}, 'MARKS': {mymark:'pos'}},
+            mutations: { UNITDATA: {} }
         },
         'when someone is there': {
             args: [['mark','mymark']],
-            scope: { 'UNITS': {7:{}}, LAYERS: {units: {pos:{id:7}}}, 'MARKS': {mymark:'pos'}},
-            mutations: { 'UNITS': {7:{dead:true}} },
+            scope: { UNITDATA: {7:{}}, LAYERS: {units: {pos:{id:7}}}, 'MARKS': {mymark:'pos'}},
+            mutations: { UNITDATA: {7:{dead:true}} },
         }
     });
     test(E.moveid,'the moveid func',{
         'for straight call': {
             args: [42,['mark','mymark']],
-            scope: { UNITS: { 42:{pos:'somepos'} }, MARKS: {mymark:'otherpos'} },
-            mutations: { UNITS: { 42: {pos:'otherpos'} } }
+            scope: { UNITDATA: { 42:{pos:'somepos'} }, MARKS: {mymark:'otherpos'} },
+            mutations: { UNITDATA: { 42: {pos:'otherpos'} } }
         }
     });
     test(E.spawn,'the spawn func',{
         'for just pos and group': {
             args: [['mark','mymark'],'fools'],
             options: {player:666},
-            scope: { UNITS: {unit1:{}}, MARKS: {mymark:'somepos'} },
-            mutations: { UNITS: {unit1:{}, unit2:{id:'unit2',group:'fools',owner:666,pos:'somepos'}} }
+            scope: { UNITDATA: {unit1:{}}, MARKS: {mymark:'somepos'} },
+            mutations: { UNITDATA: {unit1:{}, unit2:{id:'unit2',group:'fools',owner:666,pos:'somepos'}} }
         }
     });
 });
--- src/codegen/effect.js
+++ src/codegen/effect.js
@@ -2,27 +2,27 @@ import _ from "lodash";
 import C from "./core";
 
 /*
-Effect methods will all mutate UNITS, which is assumed to have been
+Effect methods will all mutate UNITDATA, which is assumed to have been
 previously copied
 */
 const effects = {
     swap: (O,pos1,pos2)=> (
         'var swappos1='+C.position(O,pos1)+'; '+
         'var swappos2='+C.position(O,pos2)+'; '+
-        '(UNITS[ (LAYERS.units[swappos1] || {}).id ] || {}).pos=swappos2;'+
-        '(UNITS[ (LAYERS.units[swappos2] || {}).id ] || {}).pos=swappos1;'
+        '(UNITDATA[ (LAYERS.units[swappos1] || {}).id ] || {}).pos=swappos2;'+
+        '(UNITDATA[ (LAYERS.units[swappos2] || {}).id ] || {}).pos=swappos1;'
     ),
-    killid: (O,id)=> "UNITS["+C.id(O,id)+"].dead=true;",
-    moveid: (O,id,pos)=> "UNITS["+C.id(O,id)+"].pos="+C.position(O,pos)+";",
-    setid: (O,id,propname,val)=> "UNITS["+C.id(O,id)+"]["+C.value(O,propname)+"]="+C.value(O,val)+";",
-    kill1at: (O,pos)=> "(UNITS[ (LAYERS.units["+C.position(O,pos)+"] || {}).id ] || {}).dead=true;",
-    move1at: (O,from,to)=> "(UNITS[ (LAYERS.units["+C.position(O,from)+"] || {}).id ] || {}).pos="+C.position(O,to)+";",
-    set1at: (O,pos,propname,val)=> "(UNITS[ (LAYERS.units["+C.position(O,pos)+"] || {}).id ] || {})["+C.value(O,propname)+"]="+C.value(O,val)+";",
+    killid: (O,id)=> "UNITDATA["+C.id(O,id)+"].dead=true;",
+    moveid: (O,id,pos)=> "UNITDATA["+C.id(O,id)+"].pos="+C.position(O,pos)+";",
+    setid: (O,id,propname,val)=> "UNITDATA["+C.id(O,id)+"]["+C.value(O,propname)+"]="+C.value(O,val)+";",
+    kill1at: (O,pos)=> "(UNITDATA[ (LAYERS.units["+C.position(O,pos)+"] || {}).id ] || {}).dead=true;",
+    move1at: (O,from,to)=> "(UNITDATA[ (LAYERS.units["+C.position(O,from)+"] || {}).id ] || {}).pos="+C.position(O,to)+";",
+    set1at: (O,pos,propname,val)=> "(UNITDATA[ (LAYERS.units["+C.position(O,pos)+"] || {}).id ] || {})["+C.value(O,propname)+"]="+C.value(O,val)+";",
     spawn: (O,pos,group,owner,obj)=>{
         let id = "ID"+(""+Math.random()).replace("\.","");
         return (
-            "let "+id+" = 'unit'+(Object.keys(UNITS).length+1);"+
-            "UNITS["+id+"] = {pos:"+C.position(O,pos)+", id:"+id+",group:"+C.value(O,group)+",owner:"+(owner ? C.value(O,owner) : O.player)+(obj?","+_.map(obj,(val,key)=>key+":"+C.value(O,val)).join(","):"")+"};"
+            "let "+id+" = 'unit'+(Object.keys(UNITDATA).length+1);"+
+            "UNITDATA["+id+"] = {pos:"+C.position(O,pos)+", id:"+id+",group:"+C.value(O,group)+",owner:"+(owner ? C.value(O,owner) : O.player)+(obj?","+_.map(obj,(val,key)=>key+":"+C.value(O,val)).join(","):"")+"};"
         );
     }
 }

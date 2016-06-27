c3d6ed8b01360f38eaaf19682fcf5183e13aa7ee
David Waller <david@krawaller.se>
2016-06-27 07:37:25 +0200

    extract filter

--- src/codegen/index.js
+++ src/codegen/index.js
@@ -2,6 +2,7 @@ import addCore from './core'
 import addEffect from './effect'
 import addGenerate from './generate'
 import addGenerateFilter from './generate_filter'
+import addGenerateNeighbour from './generate_neighbour'
 import addFlow from './flow'
 
 let o = {}
@@ -10,6 +11,7 @@ addCore(o)
 addEffect(o)
 addGenerate(o)
 addGenerateFilter(o)
+addGenerateNeighbour(o)
 addFlow(o)
 
 export default o

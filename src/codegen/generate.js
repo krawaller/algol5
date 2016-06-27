1ad0cdd90519288bd6770fdec1931b58a860a672
David Waller <david@krawaller.se>
2016-06-27 07:34:12 +0200

    extract filter

--- src/codegen/generate.js
+++ src/codegen/generate.js
@@ -5,45 +5,6 @@ import U from "../utils"
 
 export default C => Object.assign(C,{
 
-	// ------------ FILTER STUFF -----------
-
-	applyfilter: (O,def)=> {
-		let ret = ''
-		ret += 'var filtersourcelayer = '+C.layerref(O,def.layer)+'; '
-		if (!U.contains(def.tolayer,'target')){
-			ret += 'var filtertargetlayer = '+C.layerref(O,def.tolayer)+'; '
-		}
-		ret += 'for (var POS in filtersourcelayer){'
-		ret += C.filterposition(O,def)
-		ret += '}'
-		return ret
-	},
-
-	// assumes filtersourcelayer, POS,
-	filterposition: (O,def)=> {
-		let ret = ''
-		if (U.contains(def.tolayer,'target')){
-			ret += 'var filtertargetlayer = '+C.layerref(O,def.tolayer)+'; '
-		}
-		ret += 'if (filtersourcelayer[POS]){'
-		ret += 'var filterobj = filtersourcelayer[POS]; '
-		ret += C.filterobject(O,def)
-		ret += '} '
-		return ret
-	},
-
-	// assumes POS, filterobj, filtertargetlayername
-	filterobject: (O,def)=> { // TODO - core datatype for matcher?
-		let ret = ''
-		let conds = (def.condition ? [C.boolean(O,def.condition)] : [])
-		conds = conds.concat(map(def.matching,(test,key)=> C.prop(O,test,key) ))
-		ret += 'if (' + conds.join(' && ') + '){'
-		ret += C.addtolayerbyref(O,'filtertargetlayer','POS','filterobj');
-		//ret += C.addtolayer(O,'filtertargetlayername','POS','filterobj')
-		ret += '} '
-		return ret
-	},
-
 	// ------------ NEIGHBOUR STUFF -----------
 
 	applyneighbours: (O,def)=> {
--- src/codegen/index.js
+++ src/codegen/index.js
@@ -1,6 +1,7 @@
 import addCore from './core'
 import addEffect from './effect'
 import addGenerate from './generate'
+import addGenerateFilter from './generate_filter'
 import addFlow from './flow'
 
 let o = {}
@@ -8,6 +9,7 @@ let o = {}
 addCore(o)
 addEffect(o)
 addGenerate(o)
+addGenerateFilter(o)
 addFlow(o)
 
 export default o

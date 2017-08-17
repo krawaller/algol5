cc0eb2ccaf80895706fa5431e88be87f5f4e7ec7
David Waller <david@krawaller.se>
2017-08-17 17:56:08 +0200

    random moves test

--- logic/test/specs/random-moves.spec.js
+++ logic/test/specs/random-moves.spec.js
@@ -5,17 +5,24 @@ import _ from 'lodash';
 function makeRandomMovesInGame(gameId, n){
   let UI = algol.startGameSession(gameId, 'plr1', 'plr2');
   let turncount = 0;
+  let path = [];
   do {
-    let available = UI.commands.concat(UI.potentialMarks.map(m => m.pos)).concat(UI.system.filter(c => c === 'endturn'));
+    let available = UI.commands.concat(UI.potentialMarks.map(m => m.pos)).concat(UI.system.filter(c => c.substr(0,4) !== 'undo'));
     let cmnd = _.shuffle(available)[0];
     if (cmnd === 'endturn'){
       turncount++;
     }
-    UI = algol.makeSessionAction(UI.sessionId, cmnd);
-  } while (turncount < n);
+    path.push(cmnd);
+    try {
+      UI = algol.makeSessionAction(UI.sessionId, cmnd);
+    } catch(e){
+      console.log('Random moves bombed! Path', path);
+      throw e;
+    }
+  } while (turncount < n && path[path.length-1] !== 'win'); // TODO - check gamestatus in UI once implemented
 }
 
-const TURNS = 5;
+const TURNS = 10;
 
 describe(`doing ${TURNS} random moves`, ()=> {
   Object.keys(games).map(gameKey => {

a5813474c1fe7410637038f80bf7e3e1afbf101a
David Waller <david@krawaller.se>
2017-08-18 07:45:57 +0200

    refactor scripts to be more condense

--- logic/test/specs/exact.spec.js
+++ logic/test/specs/exact.spec.js
@@ -8,27 +8,23 @@ import games from '../../dist/gamelibrary';
 
 const scripts = [
   ["Basic Kickrun script", "kickrun", [
-    ["c1", ["c2", "d1"]],
-    ["c2", ["move"]],
-    ["move", ["endturn"]],
-    ["endturn", ["c5", "d5", "e3", "e4"]],
-    ["d5", ["b3", "c4", "d1", "d2", "d3", "d4"]],
-    ["d1", ["move"]],
-    ["move", ["endturn"]],
-    ["endturn", ["a2", "a3", "b1", "c2"]],
-    ["c2", ["c3", "d1", "d2"]],
-    ["d1", ["move"]],
-    ["move", ["endturn"]],
-    ["endturn", ["c5", "e3", "e4", "e5"]],
-    ["e4", ["a4", "b4", "c2", "c4", "d3", "d4"]]
+    [["c1"], ["c2", "d1"]],
+    [["c2","move","endturn"], ["c5", "d5", "e3", "e4"]],
+    [["d5"], ["b3", "c4", "d1", "d2", "d3", "d4"]],
+    [["d1", "move", "endturn"], ["a2", "a3", "b1", "c2"]],
+    [["c2"], ["c3", "d1", "d2"]],
+    [["d1","move","endturn"], ["c5", "e3", "e4", "e5"]],
+    [["e4"], ["a4", "b4", "c2", "c4", "d3", "d4"]]
   ]]
 ];
 
 describe('Following scripted moves', ()=> {
-  scripts.forEach(([name, gameId,turns], n) => it(`works for ${name}`, () => {
+  scripts.forEach(([name, gameId,lines], n) => it(`works for ${name}`, () => {
     let UI = algol.startGameSession(gameId, 'plr1', 'plr2');
-    turns.forEach(([cmnd, expectedUI]) => {
-      UI = algol.makeSessionAction(UI.sessionId, cmnd);
+    lines.forEach(([cmnds, expectedUI]) => {
+      cmnds.forEach(cmnd => {
+        UI = algol.makeSessionAction(UI.sessionId, cmnd);
+      });
       const available = UI.commands.concat(UI.potentialMarks.map(m => m.pos)).concat(UI.system.filter(c => c.substr(0,4) !== 'undo')).sort();
       expect(available).toEqual(expectedUI);
     });

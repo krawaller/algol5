3342fa3cd76deb79a9e8bd14a9a1b22df9bb094e
David Waller <david@krawaller.se>
2017-08-21 17:56:56 +0200

    murus gallicus advanced script

--- logic/test/specs/options.spec.js
+++ logic/test/specs/options.spec.js
@@ -128,6 +128,19 @@ const scripts = [
     [["e6"],["c4", "c6", "e5", "g4", "g6"]],
     [["e5","kill","endturn"],["c2", "d3", "e1", "f1", "g1", "h1"]]
   ]],
+  ["Basic Murus Gallicus Advanced script", "murusgallicusadvanced", [
+    [[],["a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1"]],
+    [["b1","d3","move","endturn"],["a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7"]],
+    [["d7","d5","move","endturn"],["a1", "c1", "d1", "e1", "f1", "g1", "h1"]],
+    [["d1","d3","move","endturn"],["a7", "b7", "c7", "e7", "f7", "g7", "h7"]],
+    [["e7","c5","move","endturn"],["a1", "c1", "d3", "e1", "f1", "g1", "h1"]],
+    [["f1","d3","move","endturn"],["a7", "b7", "c7", "d6", "f7", "g7", "h7"]],
+    [["a7","a5","move","endturn"],["a1", "c1", "d3", "e1", "g1", "h1"]],
+    [["d3"],["a3", "a6", "b3", "b5", "d5", "d6", "f3", "f5", "g3", "g6"]],
+    [["d6","fire","endturn"],["b7", "c7", "f7", "g7", "h7"]],
+    [["f7","d5","move","endturn"],["a1", "c1", "d3", "e1", "g1", "h1"]],
+    [["d3"], ["b1", "b3", "b5", "d1", "f1", "f3", "f5"]]
+  ]],
   ["Basic Transet script", "transet", [
     [[], ["a1", "b1", "c1", "d1", "e1"]],
     [["c1"], ["b2", "c2", "d2"]],

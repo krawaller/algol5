2ae8eae4af1a790f0706dd530b2d6edcd9ffcf7f
David Waller <david@krawaller.se>
2017-09-19 18:44:03 +0200

    buttons

--- app/src/parts/commands.tsx
+++ app/src/parts/commands.tsx
@@ -5,15 +5,20 @@ type CommandProps = any;
 // gameCommands, systemCommands, performCommand
 let Commands = (props: CommandProps)=> {
     let gameCommands = Object.keys(props.gameCommands).map((name)=> (
-        <button key={name} disabled={props.locked || !props.gameCommands[name]} onClick={()=>props.performCommand(name)}>{name}</button>
+        <button
+            key={name}
+            disabled={props.locked || !(props.gameCommands[name] || props.undo === name)}
+            onClick={()=>props.performCommand(props.undo === name ? "undo" : name)}
+            className="commandbtn"
+        >{props.undo === name ? "undo " + name : name}</button>
     ))
-    let systemCommands = ["undo","submit"].map((name)=> (
-        <button className={"syscommands " + name} key={name} disabled={props.locked || !props[name]} onClick={()=>props.performCommand(name === 'undo' ? name : props[name])}>{name}</button>
-    ))
+    let submitButton = (
+        <button className="submitbtn commandbtn" key={"submit"} disabled={props.locked || !props["submit"]} onClick={()=>props.performCommand(props["submit"])}>submit</button>
+    );
     /*let brainCommands = props.brains.map(name => (
         <button key={name} onClick={()=>props.askBrain(name)}>Ask {name}</button>
     ))*/
-    return <div>{gameCommands}{systemCommands}</div>
+    return <div>{gameCommands}{submitButton}</div>
 };
 
 export default Commands

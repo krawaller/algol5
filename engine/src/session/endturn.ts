487568658d88d18a8385e472b72d058b763fa682
David Waller <david@krawaller.se>
2017-10-24 07:54:27 +0200

    dont hydrate while inflating save

--- engine/src/session/endturn.ts
+++ engine/src/session/endturn.ts
@@ -8,13 +8,15 @@ import hydrateTurn from '../hydration/hydrateturn';
 export default function endTurn(session: Session, action: string) {
   session.history = session.history.concat( compressHistoryForTurn(session) );
   session.savedIndexes = session.savedIndexes.concat( calcTurnSave(session.turn,session.step,'endturn') );
-  session.turn = hydrateTurn(session.game, session.turn.next[session.step.stepid]);
-  session.saveString = encodeSessionSave({
-    gameId: session.gameId,
-    turnNumber: session.turn.turn,
-    battleId: session.battleId,
-    moveIndexes: session.savedIndexes
-  });
+  if (!session.inflating){
+    session.turn = hydrateTurn(session.game, session.turn.next[session.step.stepid]);
+    session.saveString = encodeSessionSave({
+      gameId: session.gameId,
+      turnNumber: session.turn.turn,
+      battleId: session.battleId,
+      moveIndexes: session.savedIndexes
+    });
+  }
   session.step = session.turn.steps.root;
   session.markTimeStamps = {};
   session.undo = [];

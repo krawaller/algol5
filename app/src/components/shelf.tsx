290f9fd544cc27c88ead9af2582dc89cb62418a9
David Waller <david@krawaller.se>
2017-10-27 14:09:09 +0200

    show taglines on game shelf in app

--- app/src/components/shelf.tsx
+++ app/src/components/shelf.tsx
@@ -9,6 +9,18 @@ type StateType = {
   lib: any;
 };
 
+type GameBoxProps = {
+  choose: () => void
+  meta: any
+}
+
+const GameBox = (props: GameBoxProps) => (
+  <div>
+    <button onClick={props.choose}>{props.meta.name} </button>
+    <span> - {props.meta.tagline} </span>
+  </div>
+);
+
 class Shelf extends React.Component <PropType,StateType> {
   constructor(props){
     super(props);
@@ -21,7 +33,7 @@ class Shelf extends React.Component <PropType,StateType> {
     this.props.chooseGame(this.state.lib[gamename]);
   }
   render() {
-    let choices = Object.keys(this.state.lib).map((g,n)=> <button key={n} onClick={this.chooseGame.bind(this,g)}>{g}</button>)
+    let choices = Object.keys(this.state.lib).map(g => <GameBox key={g} choose={this.chooseGame.bind(this,g)} meta={this.state.lib[g]}/>)
     const dateStr = moment(BUILT_AT).format('YYYY-MM-DD HH:mm');
     return (
       <div>

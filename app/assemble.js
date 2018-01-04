f38ec5468598ff2d1f6a95c4fbf462dd81a4d540
David Waller <david@krawaller.se>
2018-01-04 07:28:32 +0100

    make app copy piece pics

--- app/assemble.js
+++ app/assemble.js
@@ -3,6 +3,7 @@ var fs = require("fs-extra");
 fs.removeSync("dist");
 
 fs.copySync(__dirname + "/../graphics/dist/boards.css", __dirname + "/dist/boards.css");
+fs.copySync(__dirname + "/../graphics/dist/pieces", __dirname + "/dist/pieces");
 fs.copySync(__dirname + "/../engine/dist/algol_worker.js", __dirname + "/dist/algol_worker.js");
 fs.copySync(__dirname + "/styles.css", __dirname + "/dist/styles.css");
 

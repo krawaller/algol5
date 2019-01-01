ef82f285607fd6c6d8472b61f690e4d8132abab1
David <david.waller@svenskaspel.se>
2019-01-01 17:14:05 +0100

    make engine use new scripts

--- engine/test/specs/options.spec.ts
+++ engine/test/specs/options.spec.ts
@@ -7,17 +7,28 @@ import algol from '../../src';
 import optionsInUI from '../../src/various/optionsinui';
 import * as test from "tape";
 import makePlayer from '../makeplayer';
-import scripts from '../../../library/test/specs/scripts';
+import scripts from '../../../gamescripts';
 
-scripts.forEach(([name, gameId,lines], n) => {
-  test("Following scripted moves for "+name, t => {
-    t.plan(lines.length);
-    let UI = algol.startGame(gameId, makePlayer(1), makePlayer(2));
-    lines.forEach(([cmnds, expectedUI], i) => {
-      cmnds.forEach(cmnd => {
-        UI = algol.performAction(UI.sessionId, cmnd);
+Object.keys(scripts).forEach(gameId => {
+  Object.keys(scripts[gameId]).forEach(script => {
+    test("Running " + gameId + " script " + script, t => {
+      const lines = scripts[gameId][script];
+      let UI = algol.startGame(gameId, makePlayer(1), makePlayer(2));
+      lines.forEach(([cmnds, expectedOpts, forbiddenOpts=[]], i) => {
+        cmnds.forEach(cmnd => {
+          UI = algol.performAction(UI.sessionId, cmnd);
+        });
+        const opts = optionsInUI(UI,true);
+        if (expectedOpts.length) {
+          const missingOpts = expectedOpts.filter(o => opts.indexOf(o) === -1);
+          t.deepEqual(missingOpts,[],"UI after " +((cmnds.length?cmnds:["start"]).join(","))+ " included " + expectedOpts.join(","));
+        }
+        if (forbiddenOpts.length) {
+          const unwantedOpts = opts.filter(o => forbiddenOpts.indexOf(o) !== -1);
+          t.deepEqual(unwantedOpts, [], "UI after " +((cmnds.length?cmnds:["start"]).join(","))+ " didn't include " + forbiddenOpts.join(","));
+        }
       });
-      t.deepEqual(optionsInUI(UI,true), expectedUI, "executed " + ((cmnds.length?cmnds:["start"]).join(",")) + ", got UI " + expectedUI.join(","));
+      t.end();
     });
   });
 });

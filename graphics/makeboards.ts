3c671adfeb72fa0c3d1ef59ab4a5fae1a8b4b46e
David <david.waller@svenskaspel.se>
2019-01-03 07:06:01 +0100

    make graphics engine use new game defs

--- graphics/makeboards.ts
+++ graphics/makeboards.ts
@@ -4,30 +4,19 @@ Build script that will loop over all game definitions in the `defs` folder,
 and create individual background images for that game into `/boards`.
 */
 
-var META_PATH = __dirname + '/../library/dist/meta';
-var BOARD_PARTS_PATH = __dirname + '/boardparts';
-var OUTPUT_PATH = __dirname + '/dist';
-var OUTPUT_BOARD_PIC_PATH = OUTPUT_PATH + '/boards';
+import * as fs from 'fs';
+import * as Canvas from 'canvas';
 
-var fs = require('fs')
-  , Canvas = require('canvas')
-  , Image = Canvas.Image;
+import lib from '../games/dist/lib'; // must have run npm run export in the Games dir first!
+import { colnumber2name, coords2pos, terrainLayers } from '../common';
 
-// TODO - import? 
-var colnametonumber = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVXYZ".split("").reduce(function(mem,char,n) {
-	mem[char] = n+1;
-	return mem;
-},{});
-const colnumbertoname = Object.keys(colnametonumber).reduce(function(mem,key){
-  var val = colnametonumber[key];
-  mem[val] = key;
-  return mem;
-},{});
-var coords2pos = function(coords) { return colnumbertoname[coords.x]+coords.y}
+const cimg = Canvas.Image;
 
-var games = require(META_PATH);
+let BOARD_PARTS_PATH = __dirname + '/boardparts';
+let OUTPUT_PATH = __dirname + '/dist';
+let OUTPUT_BOARD_PIC_PATH = OUTPUT_PATH + '/boards';
 
-var pics = {
+let pics = {
   castle: fs.readFileSync(BOARD_PARTS_PATH + '/castle.png'),
   corner: fs.readFileSync(BOARD_PARTS_PATH + '/corner.png'),
   empty: fs.readFileSync(BOARD_PARTS_PATH + '/empty.png'),
@@ -49,10 +38,8 @@ const TILE = 200, EDGE = 75;
 let dataURIusages = {};
 
 function draw(game,to){
-  var board = game.board
-    , terraindef = board.terrain || {}
-    , layers = game.board.terrainLayers
-    , layernames = Object.keys(terraindef)
+  let board = game.board
+    , layers = terrainLayers(board)
     , tilemap = game.graphics.tiles
     , tiletypes = Object.keys(game.graphics.tiles||{})
     , height = board.height*TILE+EDGE*2
@@ -63,37 +50,37 @@ function draw(game,to){
   ctx.textAlign = 'center';
   ctx.textBaseline = 'middle';
   ctx.fillStyle = 'white';
-  var corner = new Image(); corner.src = pics.corner_nw; ctx.drawImage(corner, 0, 0, EDGE, EDGE);
-  var corner = new Image(); corner.src = pics.corner_ne; ctx.drawImage(corner, board.width*TILE + EDGE, 0, EDGE, EDGE);
-  var corner = new Image(); corner.src = pics.corner_se; ctx.drawImage(corner, board.width*TILE + EDGE, board.height*TILE + EDGE, EDGE, EDGE);
-  var corner = new Image(); corner.src = pics.corner_sw; ctx.drawImage(corner, 0, board.height*TILE + EDGE, EDGE, EDGE);
+  let corner = new cimg(); corner.src = pics.corner_nw; ctx.drawImage(corner, 0, 0, EDGE, EDGE);
+  corner = new cimg(); corner.src = pics.corner_ne; ctx.drawImage(corner, board.width*TILE + EDGE, 0, EDGE, EDGE);
+  corner = new cimg(); corner.src = pics.corner_se; ctx.drawImage(corner, board.width*TILE + EDGE, board.height*TILE + EDGE, EDGE, EDGE);
+  corner = new cimg(); corner.src = pics.corner_sw; ctx.drawImage(corner, 0, board.height*TILE + EDGE, EDGE, EDGE);
 
-  for (var col=1; col <= board.width; col++){
-    var colHeader = new Image();
+  for (let col=1; col <= board.width; col++){
+    let colHeader = new cimg();
     colHeader.src = pics.colheader;
     ctx.drawImage(colHeader, EDGE + (col-1)*TILE, 0, TILE, EDGE);
     ctx.drawImage(colHeader, EDGE + (col-1)*TILE, board.height * TILE + EDGE, TILE, EDGE);
-    ctx.fillText(colnumbertoname[col], (col-1)*TILE + EDGE + TILE/2, EDGE/2);
-    ctx.fillText(colnumbertoname[col], (col-1)*TILE + EDGE + TILE/2, board.height * TILE + EDGE * 3/2);
+    ctx.fillText(colnumber2name[col], (col-1)*TILE + EDGE + TILE/2, EDGE/2);
+    ctx.fillText(colnumber2name[col], (col-1)*TILE + EDGE + TILE/2, board.height * TILE + EDGE * 3/2);
   }
-  for(var row = 1; row <= board.height; row++){
-    var picY = EDGE + (board.height-row)*TILE
-    var rowHeader = new Image();
+  for(let row = 1; row <= board.height; row++){
+    let picY = EDGE + (board.height-row)*TILE
+    let rowHeader = new cimg();
     rowHeader.src = pics.rowheader;
     ctx.drawImage(rowHeader, 0, picY, TILE, TILE);
     ctx.drawImage(rowHeader, board.width * TILE, picY, TILE, TILE);
     ctx.fillStyle = 'white';
     ctx.fillText(row, EDGE/2, picY + (TILE/2));
     ctx.fillText(row, board.width * TILE + EDGE * 3/2, picY + (TILE/2));
-    for (var col=1; col <= board.width; col++){
-      var pos = coords2pos({x:col,y:row})
-      var dark = !((col+(row%2))%2)
-      var sqr = tiletypes.reduce(function(mem,name){
+    for (let col=1; col <= board.width; col++){
+      let pos = coords2pos({x:col,y:row})
+      let dark = !((col+(row%2))%2)
+      let sqr = tiletypes.reduce(function(mem,name){
         return layers[name][pos] ? tilemap[name]==='playercolour' ? {1:'player1base',2:'player2base'}[layers[name][pos].owner] : tilemap[name] : mem;
       },'empty');
-      var img = new Image()
+      let img = new cimg()
       img.src = pics[sqr]
-      var picX = EDGE + (col-1)*TILE
+      let picX = EDGE + (col-1)*TILE
       ctx.drawImage(img, picX, picY, TILE, TILE);
       if (dark){
         ctx.globalAlpha = 0.05
@@ -104,7 +91,7 @@ function draw(game,to){
     }
   }
 
-  var out = fs.createWriteStream(to)
+  let out = fs.createWriteStream(to)
     , stream = canvas.pngStream();
 
   stream.on('data', function(chunk){ out.write(chunk); });
@@ -112,18 +99,18 @@ function draw(game,to){
 
   let dataURI = canvas.toDataURL('image/png');
   dataURIusages[dataURI] = dataURIusages[dataURI] || [];
-  dataURIusages[dataURI].push(game.id);
+  dataURIusages[dataURI].push(game.meta.id);
 }
 
-Object.keys(games).forEach(function(game){
-  var rules = games[game];
-  draw(rules,OUTPUT_BOARD_PIC_PATH + '/'+game+'.png');
+Object.keys(lib).forEach(function(gameId){
+  let def = lib[gameId];
+  draw(def,OUTPUT_BOARD_PIC_PATH + '/'+gameId+'.png');
 });
 
-var CSS = Object.keys(dataURIusages).reduce(function(mem, uri){
-  var gamelist = dataURIusages[uri];
+let CSSfile = Object.keys(dataURIusages).reduce(function(mem, uri){
+  let gamelist = dataURIusages[uri];
   mem += gamelist.map(function(id){ return "."+id; }).join(", ") + " { background-image: url(" + uri + "); }\n";
   return mem;
 }, '');
 
-fs.writeFileSync(OUTPUT_PATH + '/boards.css', CSS);
+fs.writeFileSync(OUTPUT_PATH + '/boards.css', CSSfile);

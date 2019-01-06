5c8b5a3cd6eb3ae47703c54fbb2682343248f708
David <david.waller@svenskaspel.se>
2019-01-06 06:18:37 +0100

    make engine use scripts from games

--- engine/test/specs/options.spec.ts
+++ engine/test/specs/options.spec.ts
@@ -3,29 +3,45 @@ This test executes scipted steps, for each making sure that the available option
 in the resulting UI is exactly what is expected (excluding undos and removeMarks).
 */
 
-import algol from '../../src';
-import optionsInUI from '../../src/various/optionsinui';
+import algol from "../../src";
+import optionsInUI from "../../src/various/optionsinui";
 import * as test from "tape";
-import makePlayer from '../makeplayer';
-import scripts from '../../../gamescripts';
+import makePlayer from "../makeplayer";
+import defs from "../../../games/dist/lib";
 
-Object.keys(scripts).forEach(gameId => {
-  Object.keys(scripts[gameId]).forEach(script => {
+Object.keys(defs).forEach(gameId => {
+  Object.keys(defs[gameId].scripts).forEach(script => {
     test("Running " + gameId + " script " + script, t => {
-      const lines = scripts[gameId][script];
+      const lines = defs[gameId].scripts[script];
       let UI = algol.startGame(gameId, makePlayer(1), makePlayer(2));
-      lines.forEach(([cmnds, expectedOpts, forbiddenOpts=[]], i) => {
+      lines.forEach(([cmnds, expectedOpts, forbiddenOpts = []], i) => {
         cmnds.forEach(cmnd => {
           UI = algol.performAction(UI.sessionId, cmnd);
         });
-        const opts = optionsInUI(UI,true);
+        const opts = optionsInUI(UI, true);
         if (expectedOpts.length) {
           const missingOpts = expectedOpts.filter(o => opts.indexOf(o) === -1);
-          t.deepEqual(missingOpts,[],"UI after " +((cmnds.length?cmnds:["start"]).join(","))+ " included " + expectedOpts.join(","));
+          t.deepEqual(
+            missingOpts,
+            [],
+            "UI after " +
+              (cmnds.length ? cmnds : ["start"]).join(",") +
+              " included " +
+              expectedOpts.join(",")
+          );
         }
         if (forbiddenOpts.length) {
-          const unwantedOpts = opts.filter(o => forbiddenOpts.indexOf(o) !== -1);
-          t.deepEqual(unwantedOpts, [], "UI after " +((cmnds.length?cmnds:["start"]).join(","))+ " didn't include " + forbiddenOpts.join(","));
+          const unwantedOpts = opts.filter(
+            o => forbiddenOpts.indexOf(o) !== -1
+          );
+          t.deepEqual(
+            unwantedOpts,
+            [],
+            "UI after " +
+              (cmnds.length ? cmnds : ["start"]).join(",") +
+              " didn't include " +
+              forbiddenOpts.join(",")
+          );
         }
       });
       t.end();

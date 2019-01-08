7216a9f1a60c9e5a2cbad6374f5e428a76320312
David <david.waller@svenskaspel.se>
2019-01-08 10:49:00 +0100

    fix logic meta command

--- modules/logic/makemeta.ts
+++ modules/logic/makemeta.ts
@@ -9,22 +9,13 @@ import * as fs from "fs";
 import * as omit from "lodash/omit";
 import * as _eval from "eval";
 
-import makeParser from "./def2code/expressions";
-
-const envelope = fs.readFileSync(__dirname + "/envelope.js").toString();
-
-let terrainLayers = _eval(envelope + "  module.exports = terrainLayers;");
-
-const flat = ["flow"];
-const perline = ["concepts"];
-const perobj = ["actions", "tiles", "units", "goals"];
+import { terrainLayers } from '../common';
 
 import defs from "../games/dist/lib";
 
 let meta = Object.keys(defs).reduce((mem, gamename) => {
   console.log("Meta for", gamename);
   let json = defs[gamename];
-  const parse = makeParser(json, 1, "rules");
   let id = gamename.replace(".json", "");
   let allTerrainLayers = terrainLayers(json.board);
   mem[id] = {
@@ -41,39 +32,6 @@ let meta = Object.keys(defs).reduce((mem, gamename) => {
     },
     graphics: json.graphics
   };
-  if (json.meta.rules) {
-    // TODO - remove once all have
-    const rules = json.meta.rules;
-    mem[id].rules = {};
-    flat.forEach(
-      key => (mem[id].rules[key] = eval(envelope + parse.content(rules[key])))
-    );
-    perline.forEach(key => {
-      if (rules[key]) {
-        mem[id].rules[key] = Object.keys(rules[key]).reduce(
-          (m, name) => ({
-            ...m,
-            [name]: eval(envelope + parse.content(rules[key][name]))
-          }),
-          {}
-        );
-      }
-    });
-    perobj.forEach(key => {
-      if (rules[key]) {
-        mem[id].rules[key] = Object.keys(rules[key]).reduce(
-          (m, name) => ({
-            ...m,
-            [name]: {
-              rule: eval(envelope + parse.content(rules[key][name].rule)),
-              who: rules[key][name].who
-            }
-          }),
-          {}
-        );
-      }
-    });
-  }
   return mem;
 }, {});
 

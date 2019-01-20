2c44f8b5c2c5bccc08724763926c3791be41669f
David <david.waller@svenskaspel.se>
2019-01-20 06:36:08 +0100

    inlude AI when stubbing new games

--- modules/games/commands/helpers/templates/ai.ts
+++ modules/games/commands/helpers/templates/ai.ts
@@ -1,8 +1,14 @@
 export default function templateAI(gameId) {
   const capId = gameId[0].toUpperCase().concat(gameId.slice(1));
-  return `import { ${capId}AI } from './_types';
+  return `import { ${capId}AI } from "./_types";
 
-const ${gameId}AI: ${capId}AI = {};
+const ${gameId}AI: ${capId}AI = {
+  brains: {},
+  generators: {},
+  aspects: {},
+  grids: {},
+  terrain: {}
+};
 
 export default ${gameId}AI;
 `;

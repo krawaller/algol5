1a5f29c374bd583cdea711366a67b9855f421593
David <david.waller@svenskaspel.se>
2019-01-29 20:44:14 +0100

    set layer reference

--- modules/logic/def2code2/expressions/set.ts
+++ modules/logic/def2code2/expressions/set.ts
@@ -1,4 +1,5 @@
 import { FullDefAnon, AlgolSetAnon, isAlgolSetSingle } from "../../../types";
+import { artifactLayers, terrainLayers } from "../../../common";
 
 import makeParser from "./";
 
@@ -10,6 +11,18 @@ export default function parseSet(
   from?: string
 ) {
   const parser = makeParser(gameDef, player, action, "set");
+
+  if (typeof expr === "string") {
+    if (artifactLayers(gameDef.generators)[expr]) {
+      return `ARTIFACTS.${expr}`;
+    }
+    if (terrainLayers(gameDef.board, 1)[expr]) {
+      return `TERRAIN.${expr}`;
+    }
+    if (expr === "board" || expr === "light" || expr === "dark") {
+      return `BOARD.${expr}`;
+    }
+  }
 
   if (isAlgolSetSingle(expr)) {
     const { single: pos } = expr;
--- modules/logic/newlogictests/set.spec.ts
+++ modules/logic/newlogictests/set.spec.ts
@@ -11,6 +11,20 @@ const tests: ParserTest<AlgolSetAnon>[] = [
     action: "someaction",
     contexts: [
       {
+        context: {
+          BOARD: {
+            board: { b: "oard" },
+            light: { l: "ight" },
+            dark: { d: "ark" }
+          }
+        },
+        tests: [
+          { expr: "board", res: { b: "oard" } },
+          { expr: "light", res: { l: "ight" } },
+          { expr: "dark", res: { d: "ark" } }
+        ]
+      },
+      {
         context: { MARKS: { mymark: "h9" } },
         tests: [
           {
@@ -18,6 +32,39 @@ const tests: ParserTest<AlgolSetAnon>[] = [
             res: { h9: 1 }
           }
         ]
+      }
+    ]
+  },
+  {
+    def: {
+      ...emptyFullDef,
+      board: {
+        ...emptyFullDef.board,
+        terrain: {
+          boo: { 1: ["a1"], 2: ["b2"] }
+        }
+      },
+      generators: {
+        foo: {
+          type: "walker",
+          draw: {
+            start: {
+              tolayer: "gnurp"
+            }
+          }
+        }
+      }
+    },
+    player: 1,
+    action: "someaction",
+    contexts: [
+      {
+        context: { ARTIFACTS: { gnurp: { g1: { gnurp: 1 } } } },
+        tests: [{ expr: "gnurp", res: { g1: { gnurp: 1 } } }]
+      },
+      {
+        context: { TERRAIN: { myboo: { a1: { boo: 1 } } } },
+        tests: [{ expr: "myboo", res: { a1: { boo: 1 } } }]
       }
     ]
   }

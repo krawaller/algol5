b167165791340920e83f2fce29ce9ea74220f6ad
David <david.waller@svenskaspel.se>
2019-01-31 14:46:15 +0100

    deduceInitialUnitData spec

---
+++ modules/common/units/deduceInitialUnitData.spec.ts
@@ -0,0 +1,61 @@
+import * as test from "tape";
+import { SetupAnon } from "../../types";
+import { deduceInitialUnitData } from "../";
+
+type unitDataTest = {
+  setup: SetupAnon;
+  expected: {
+    [unitid: string]: {
+      [prop: string]: string | number;
+    };
+  };
+};
+
+const unitLayerTests: unitDataTest[] = [
+  {
+    setup: {
+      dorks: {
+        1: ["a1"],
+        2: ["b1", { datasites: [{ strength: 3 }, "c1"] }]
+      }
+    },
+    expected: {
+      unit1: {
+        id: "unit1",
+        owner: 1,
+        group: "dorks",
+        pos: "a1",
+        x: 1,
+        y: 1
+      },
+      unit2: {
+        id: "unit2",
+        owner: 2,
+        group: "dorks",
+        pos: "b1",
+        x: 2,
+        y: 1
+      },
+      unit3: {
+        id: "unit3",
+        owner: 2,
+        group: "dorks",
+        pos: "c1",
+        x: 3,
+        y: 1,
+        strength: 3
+      }
+    }
+  }
+];
+
+test("deduceInitialUnitData", t => {
+  unitLayerTests.forEach(({ setup, expected }) =>
+    t.deepEqual(
+      deduceInitialUnitData(setup),
+      expected,
+      `Correctly calculates data for for ${JSON.stringify(setup)}`
+    )
+  );
+  t.end();
+});

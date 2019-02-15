82f91948e5e8d7ab4d8b4e8534b1a9937fd28ec7
David <david.waller@svenskaspel.se>
2019-02-15 19:32:57 +0100

    fix faulty setup in Aries

--- modules/games/definitions/aries/setup.ts
+++ modules/games/definitions/aries/setup.ts
@@ -1,13 +1,9 @@
-import { AriesSetup } from './_types';
+import { AriesSetup } from "./_types";
 
 const ariesSetup: AriesSetup = {
   soldiers: {
-    "1": [
-      ["rect", "a1", "d4"]
-    ],
-    "2": [
-      ["rect", "e5", "h8"]
-    ]
+    "1": [{ rect: ["a1", "d4"] }],
+    "2": [{ rect: ["e5", "h8"] }]
   }
 };
 

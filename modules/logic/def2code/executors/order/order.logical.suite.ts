d5aff2fa379f192294d0c68d303547b93b6c1a80
David <david.waller@svenskaspel.se>
2019-03-18 07:27:04 +0100

    logical order suite

---
+++ modules/logic/def2code/executors/order/order.logical.suite.ts
@@ -0,0 +1,48 @@
+import { executeOrder } from "..";
+import { emptyFullDef, truthy } from "../../../../common";
+import { AlgolStatementSuite, AlgolOrderAnon } from "../../../../types";
+
+export const testSuite: AlgolStatementSuite<AlgolOrderAnon> = {
+  title: "Section - Orders",
+  func: executeOrder,
+  defs: [
+    {
+      def: emptyFullDef,
+      player: 1,
+      action: "somemark",
+      contexts: [
+        {
+          context: {
+            newStepId: "foo",
+            UNITDATA: { unit1: {} },
+            turn: { links: { foo: {} } }
+          },
+          tests: [
+            {
+              expr: { multi: [ { effects: [{ killid: 'unit1' }] }, { links: ["endturn"] } ] },
+              asserts: [
+                {
+                  sample: "turn.links.foo",
+                  res: { endturn: "start2" }
+                },
+                {
+                  sample: "UNITDATA.unit1",
+                  res: undefined
+                }
+              ]
+            },
+            {
+              expr: {   if: [["false"], { effects: [{ killid: 'unit1' }]}] },
+              asserts: [
+                {
+                  sample: "UNITDATA.unit1",
+                  res: truthy
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    }
+  ]
+};

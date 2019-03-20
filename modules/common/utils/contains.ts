56c23dbd73896068d10f143de16932d5de2b1049
David <david.waller@svenskaspel.se>
2019-03-20 07:08:18 +0100

    make contains take checker

--- modules/common/utils/contains.ts
+++ modules/common/utils/contains.ts
@@ -3,7 +3,9 @@ import * as isEqual from "lodash.isequal";
 export function contains(haystack, needle): boolean {
   if (!haystack) {
     return false;
-  } else if (isEqual(needle, haystack)) {
+  } else if (
+    typeof needle === "function" ? needle(haystack) : isEqual(needle, haystack)
+  ) {
     return true;
   } else if (typeof haystack === "object") {
     return Object.keys(haystack).some(key => contains(haystack[key], needle));

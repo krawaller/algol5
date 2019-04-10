9f4bbedb4ccc313ab2f86dbdb829d0c4d29ff817
David Waller <david@krawaller.se>
2019-04-10 07:23:11 +0200

    remove old reduce common util

--- modules/common/utils/index.ts
+++ modules/common/utils/index.ts
@@ -1,4 +1,3 @@
 export * from "./emptyFullDef";
-export * from "./reduce";
 export * from "./possibilities";
 export * from "./contains";
--- modules/common/utils/reduce.ts
+++
@@ -1,6 +0,0 @@
-export function reduce(coll, iterator, acc) {
-  for (var key in coll) {
-    acc = iterator(acc, coll[key], key);
-  }
-  return acc;
-}

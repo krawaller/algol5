afb12ae0ed2be8d408814d88d169b27bf9ad5607
David Waller <david@krawaller.se>
2019-04-13 20:37:14 +0200

    extract parseOffset

--- modules/common/connections/index.ts
+++ modules/common/connections/index.ts
@@ -1,3 +1,4 @@
 export * from "./posConnections";
 export * from "./boardConnections";
 export * from "./makeRelativeDirs";
+export * from "./parseOffset";
---
+++ modules/common/connections/parseOffset.ts
@@ -0,0 +1,20 @@
+import { AlgolOffsetBasic, AlgolOffset } from "../../types";
+
+export function parseOffset(offset: AlgolOffset): AlgolOffsetBasic[] {
+  if (Array.isArray(offset)) {
+    const [dirs, forward, right] = offset;
+    return [[parseDirs(dirs), forward, right]];
+  }
+  if (offset === "knight") {
+    return parseOffset(["ortho", 2, 1]).concat(parseOffset(["ortho", 2, -1]));
+  }
+  throw new Error(`Unknown offset def: ${JSON.stringify(offset)}`);
+}
+
+function parseDirs(dirs) {
+  if (Array.isArray(dirs)) return dirs;
+  if (dirs === "ortho") return [1, 3, 5, 7];
+  if (dirs === "diag") return [2, 4, 6, 8];
+  if (dirs === "rose") return [1, 2, 3, 4, 5, 6, 7, 8];
+  throw new Error(`Unknown offset dirs: ${JSON.stringify(dirs)}`);
+}

beb69c4728553331b5f91b43c251777e1ecaeb61
David Waller <dwaller@apple.com>
2019-04-26 15:25:59 +0200

    add all api file when exporting all battle engines

--- modules/battle/commands/exportGameAPIs.ts
+++ modules/battle/commands/exportGameAPIs.ts
@@ -1,15 +1,23 @@
 import { exportGameAPI } from "./helpers/exportGameAPI";
 import names, { GameId } from "./helpers/_names";
+import * as fs from "fs-extra";
+import * as path from "path";
 
 const gameId = <GameId>process.argv[2];
 
-if (!gameId) {
-  console.log("---- Exporting API for all games ----");
-  Promise.all(names.map(exportGameAPI)).then(() =>
-    console.log("---- All games got APIs! ----")
-  );
-} else if (names.indexOf(gameId) === -1) {
-  console.log(`Game "${gameId}" doesn't exists!`);
-} else {
-  exportGameAPI(gameId).catch(e => console.log("API export failed", e));
-}
+(async () => {
+  if (!gameId) {
+    console.log("---- Exporting API for all games ----");
+    await Promise.all(names.map(exportGameAPI));
+    await fs.writeFile(
+      path.join(__dirname, "../dist", "allAPIs.tsx"),
+      `${names.map(id => `import ${id} from './apis/${id}';\n`).join("")}
+export default { ${names.join(", ")} };`
+    );
+    console.log("---- All games got APIs! ----");
+  } else if (names.indexOf(gameId) === -1) {
+    console.log(`Game "${gameId}" doesn't exists!`);
+  } else {
+    exportGameAPI(gameId).catch(e => console.log("API export failed", e));
+  }
+})();

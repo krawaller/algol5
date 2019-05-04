70ea01293411c27bb69c17e01d20ddfb60048556
David Waller <dwaller@apple.com>
2019-05-04 14:22:44 +0200

    anim per cmnd

--- modules/types/gamedef/anim.ts
+++ modules/types/gamedef/anim.ts
@@ -9,4 +9,32 @@ export type AlgolAnim<
   Turnp extends string,
   Turnv extends string,
   Unit extends string
+> = Partial<
+  {
+    [cmnd in Cmnd]: AlgolAnimCmnd<
+      Btlp,
+      Btlv,
+      Cmnd,
+      Grid,
+      Layer,
+      Mrk,
+      Phase,
+      Turnp,
+      Turnv,
+      Unit
+    >
+  }
+>;
+
+export type AlgolAnimCmnd<
+  Btlp extends string,
+  Btlv extends string,
+  Cmnd extends string,
+  Grid extends string,
+  Layer extends string,
+  Mrk extends string,
+  Phase extends string,
+  Turnp extends string,
+  Turnv extends string,
+  Unit extends string
 > = {};

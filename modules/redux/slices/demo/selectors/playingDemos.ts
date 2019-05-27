8626e77beed370f4928832bf7fc565e9a7466b4f
David Waller <dwaller@apple.com>
2019-05-27 07:23:34 +0200

    improve demo selector cacheing

--- modules/redux/slices/demo/selectors/playingDemos.ts
+++ modules/redux/slices/demo/selectors/playingDemos.ts
@@ -1,6 +1,8 @@
 import { WithAlgolDemoState } from "../types";
+import { createSelector } from "reselect";
 
-export const playingDemos = (state: WithAlgolDemoState) =>
-  Object.keys(state.demo.demos).filter(
-    gameId => state.demo.demos[gameId].playing
-  ).length;
+export const playingDemos = createSelector(
+  (state: WithAlgolDemoState) => state.demo.demos,
+  (demos: WithAlgolDemoState["demo"]["demos"]) =>
+    Object.keys(demos).filter(gameId => demos[gameId].playing).length
+);

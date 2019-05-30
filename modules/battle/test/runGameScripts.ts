4be7b5668bb2f84649dab2f8096315474f5d294b
David Waller <dwaller@apple.com>
2019-05-30 09:22:09 +0200

    fix gameScript bug

--- modules/battle/test/runGameScripts.ts
+++ modules/battle/test/runGameScripts.ts
@@ -15,7 +15,7 @@ export function runGameScripts(
       let { initialUI: ui, performAction } = api.newBattle();
       for (const action of seq) {
         ui =
-          action === "win" || action === "endturn"
+          action === "win" || action === "endturn" || action === "endTurn"
             ? performAction("endTurn")
             : identifyMark.test(action)
             ? performAction("mark", action)

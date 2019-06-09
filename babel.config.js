39d29aeb2f8a02f51f640e337ebf9b2e84aa265b
David Waller <dwaller@apple.com>
2019-06-09 17:12:24 +0200

    storybook deps mess

--- babel.config.js
+++ babel.config.js
@@ -2,12 +2,19 @@ module.exports = {
   presets: [
     // Compile to environments listed in .browserslistrc
     "@babel/env",
-    "@babel/typescript"
+    "@babel/typescript",
+    [
+      "@babel/preset-env",
+      {
+        useBuiltIns: "entry",
+        corejs: 2,
+      },
+    ],
   ],
   plugins: [
     // class { handleThing = () => { } }
     "@babel/proposal-class-properties",
     // { ...spread }
-    "@babel/proposal-object-rest-spread"
-  ]
+    "@babel/proposal-object-rest-spread",
+  ],
 };

e363ad6272066f3c7ff41c84fbb3a38070a94ebe
David Waller <dwaller@apple.com>
2019-06-19 09:42:44 +0200

    use redux in demo test component

--- modules/ui/src/Demo/index.tsx
+++ modules/ui/src/Demo/index.tsx
@@ -1,56 +1,43 @@
-import * as React from "react";
-import {
-  AlgolDemoControl,
-  AlgolDemo,
-  AlgolAnimCompiled,
-  AlgolArmy
-} from "../../../types";
-
+import React, { useEffect, useState } from "react";
+import { AlgolDemo } from "../../../types";
+import { GameId } from "../../../games/dist/list";
+import { makeStore } from "../../../redux/store";
+import { AlgolGameDemoState, stopAllDemos } from "../../../redux/slices";
+import { playDemo } from "../../../redux/slices/demo/thunks/playDemo";
 import { Board } from "../Board";
-
-import { playDemo } from "../../../common";
+import dataURIs from "../../../graphics/dist/svgDataURIs";
+import { emptyAnim } from "../../../common";
 
 type DemoProps = {
+  gameId: GameId;
   demo: AlgolDemo;
-  board: { height: number; width: number; dataURI: string };
-};
-type DemoState = {
-  marks: string[];
-  potentialMarks: string[];
-  units: AlgolArmy;
-  anim: AlgolAnimCompiled;
 };
 
-export class Demo extends React.Component<DemoProps, DemoState> {
-  stopDemo: AlgolDemoControl["stop"];
-  constructor(props: DemoProps) {
-    super(props);
-    const { stop, initial } = playDemo(props.demo, cur => {
-      this.setState({ units: cur.army, anim: cur.anim });
-    });
-    this.stopDemo = stop;
-    this.state = {
-      marks: [],
-      potentialMarks: [],
-      units: initial.army,
-      anim: initial.anim
-    };
-  }
-  componentWillUnmount() {
-    this.stopDemo();
-  }
-  handleBoardClick = () => {};
-  render() {
-    const { marks, potentialMarks, units, anim } = this.state;
-    return (
-      <Board
-        board={this.props.board}
-        marks={marks}
-        potentialMarks={potentialMarks}
-        callback={this.handleBoardClick}
-        units={units}
-        anim={anim}
-      />
+export const Demo = (props: DemoProps) => {
+  const { gameId, demo } = props;
+  const [gameDemoState, setGameDemoState] = useState<null | AlgolGameDemoState>(
+    null
+  );
+  useEffect(() => {
+    const store = makeStore();
+    store.subscribe(() =>
+      setGameDemoState(store.getState().demo.demos[gameId]!)
     );
-  }
-}
+    store.dispatch(playDemo(gameId, demo));
+    return () => {
+      store.dispatch(stopAllDemos());
+    };
+  }, [gameId]);
+  if (!gameDemoState) return null;
+  console.log(gameDemoState);
+  return (
+    <Board
+      board={dataURIs[gameId]}
+      marks={[]}
+      potentialMarks={[]}
+      callback={() => {}}
+      units={gameDemoState.positions[gameDemoState.frame]}
+      anim={{ ...emptyAnim, ...gameDemoState.anims[gameDemoState.frame] }}
+    />
+  );
+};

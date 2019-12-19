65650716851eade051691274a6064b142de13764
David Waller <dwaller@apple.com>
2019-12-19 14:54:06 +0100

    remove glitz

--- modules/ui/src/_helpers/glitz.ts
+++
@@ -1,3 +0,0 @@
-import { GlitzClient } from "@glitz/core";
-import transformers from "@glitz/transformers";
-export const glitz = new GlitzClient({ transformer: transformers() });
--- modules/ui/src/_helpers/index.ts
+++ modules/ui/src/_helpers/index.ts
@@ -1,2 +1,1 @@
 export * from "./positionStyles";
-export * from "./glitz";

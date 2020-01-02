dc9d048459cbed1f02b98a7017bdc88afe697b98
David Waller <dwaller@apple.com>
2020-01-02 09:31:26 +0100

    maintain session in useBattle hook

--- modules/ui/src/helpers/useBattle.ts
+++ modules/ui/src/helpers/useBattle.ts
@@ -1,5 +1,9 @@
-import { useReducer } from "react";
-import { AlgolStaticGameAPI, AlgolBattle } from "../../../types";
+import { useReducer, useRef } from "react";
+import {
+  AlgolStaticGameAPI,
+  AlgolBattle,
+  AlgolLocalBattle,
+} from "../../../types";
 
 type BattleAction = "mark" | "command" | "endTurn" | "undo" | "toFrame" | "new";
 type BattleCmnd = [BattleAction, any];
@@ -10,6 +14,7 @@ type BattleHookState = {
 };
 
 export function useBattle(api: AlgolStaticGameAPI) {
+  const session = useRef<null | AlgolLocalBattle>(null);
   return useReducer(
     (state: BattleHookState, instr: BattleCmnd) => {
       const [cmnd, arg] = instr;
@@ -19,19 +24,51 @@ export function useBattle(api: AlgolStaticGameAPI) {
           frame: arg,
         };
       } else if (cmnd === "new") {
+        const battle = api.newBattle();
+        session.current = {
+          id: Math.random().toString(),
+          created: Date.now(),
+          updated: Date.now(),
+          save: {
+            player: 1,
+            turn: 1,
+            path: [],
+          },
+          screenshot: {
+            marks: [],
+            units: battle.state.board.units,
+          },
+        };
         return {
-          battle: api.newBattle(),
+          battle,
           frame: 1,
         };
       } else {
-        const newBattle = api.performAction(state.battle!, cmnd, instr[1]);
+        const battle = api.performAction(state.battle!, cmnd, instr[1]);
+        const frame = battle.gameEndedBy
+          ? battle.history.length - 1
+          : cmnd === "endTurn"
+          ? battle.history.length
+          : state.frame;
+        if (cmnd === "endTurn") {
+          session.current = {
+            ...session.current!,
+            updated: Date.now(),
+            screenshot: {
+              marks: battle.state.board.marks,
+              units: battle.state.board.units,
+            },
+            save: {
+              endedBy: battle.gameEndedBy,
+              turn: battle.turnNumber,
+              player: battle.gameEndedBy ? battle.winner! : battle.player,
+              path: battle.path,
+            },
+          };
+        }
         return {
-          frame: newBattle.gameEndedBy
-            ? newBattle.history.length - 1
-            : cmnd === "endTurn"
-            ? newBattle.history.length
-            : state.frame,
-          battle: newBattle,
+          frame,
+          battle,
         };
       }
     },

03052b284d4be7747522e92c6503a423b9227f4e
David Waller <dwaller@apple.com>
2020-01-07 11:55:10 +0100

    add modal to title page

--- modules/ui/src/components/TitlePage/TitlePage.tsx
+++ modules/ui/src/components/TitlePage/TitlePage.tsx
@@ -2,15 +2,21 @@
  * Used in the Next app as the main Index page for the app
  */
 
-import React, { FunctionComponent, useCallback, Fragment } from "react";
+import React, {
+  FunctionComponent,
+  useCallback,
+  Fragment,
+  useState,
+} from "react";
 import { GameList } from "../GameList";
 import { usePrefetchGames } from "./TitlePage.prefetch";
-
+import ReactModal from "react-modal";
 import { PageActions } from "../../helpers";
-import { GameId } from "../../../../games/dist/list";
+import { GameId, list } from "../../../../games/dist/list";
 
 import { Page } from "../Page";
 import base64TitlePic from "../../../base64/title.png.proxy";
+import styles from "./TitlePage.cssProxy";
 
 type TitlePageProps = {
   actions: PageActions;
@@ -23,6 +29,13 @@ export const TitlePage: FunctionComponent<TitlePageProps> = props => {
     [actions]
   );
   usePrefetchGames(actions);
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
   return (
     <Page
       top={<img src={base64TitlePic} />}
@@ -30,11 +43,27 @@ export const TitlePage: FunctionComponent<TitlePageProps> = props => {
       body={
         <Fragment>
           <p>
-            Welcome! This is rather bare bones still, but, click a game below to
-            try it out!
+            Congratulations! You've found the AWESOMEST abstract board game site
+            there is! We've got {list.length} games and counting!
           </p>
-          <hr />
-          <GameList callback={navToGame} />
+          <div className={styles.titlePageButtonContainer}>
+            <button onClick={openModal}>Play a game</button>
+            <button
+              onClick={() => alert("It rules! What more do u need to know?")}
+            >
+              About the site
+            </button>
+          </div>
+          <ReactModal isOpen={isModalOpen} onRequestClose={closeModal}>
+            <button
+              onClick={closeModal}
+              className={styles.titlePageModalCloser}
+            >
+              X
+            </button>
+            <p>Pick your poison! :D</p>
+            <GameList callback={navToGame} />
+          </ReactModal>
         </Fragment>
       }
     />

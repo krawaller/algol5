4e2cad60abae4bb399eee6248324acccf12256e2
David Waller <dwaller@apple.com>
2020-01-07 18:29:19 +0100

    add homemade modal component

---
+++ modules/ui/src/components/Modal/Modal.tsx
@@ -0,0 +1,34 @@
+import React, { FunctionComponent, useEffect } from "react";
+import { UniversalPortal } from "@jesstelford/react-portal-universal";
+import css from "./Modal.cssProxy";
+
+type ModalProps = {
+  isOpen: boolean;
+  onClose: () => void;
+  title: string;
+};
+
+export const Modal: FunctionComponent<ModalProps> = props => {
+  const { isOpen, onClose, title, children } = props;
+  useEffect(() => {
+    const onEsc = (e: KeyboardEvent) => e.key === "Escape" && onClose();
+    window.addEventListener("keydown", onEsc);
+    return () => window.removeEventListener("keydown", onEsc);
+  }, [onClose]);
+  if (!isOpen) return null;
+  return (
+    <UniversalPortal selector="body">
+      <div className={css.modalOverlay} onClick={onClose}>
+        <div onClick={e => e.stopPropagation()} className={css.modalContainer}>
+          <div className={css.modalHeader}>
+            {title}
+            <button className={css.modalCloseButton} onClick={onClose}>
+              X
+            </button>
+          </div>
+          <div className={css.modalContent}>{children}</div>
+        </div>
+      </div>
+    </UniversalPortal>
+  );
+};

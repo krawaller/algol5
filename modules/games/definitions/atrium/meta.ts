8d5ae12925d456a97ef11ca7eff97c3d9bf26190
David Waller <dwaller@apple.com>
2020-01-12 09:19:18 +0100

    fix error in atrium tagline

--- modules/games/definitions/atrium/meta.ts
+++ modules/games/definitions/atrium/meta.ts
@@ -1,4 +1,4 @@
-import { AtriumMeta } from './_types';
+import { AtriumMeta } from "./_types";
 
 const atriumMeta: AtriumMeta = {
   id: "atrium",
@@ -7,7 +7,7 @@ const atriumMeta: AtriumMeta = {
   tags: ["formation", "nocapture"],
   author: "Guy Jeandel",
   year: 2004,
-  tagline: "Be the first to form a line with one of your three unit types",
+  tagline: "Be the first to form a line with one of your unit types",
 };
 
 export default atriumMeta;

3e8cb6ef37855b4a31d31b55b58f4907d0124eac
David Waller <dwaller@apple.com>
2020-01-13 17:51:26 +0000

    update save goal handling

--- modules/battle/src/battle/helpers/makeBattleSave.ts
+++ modules/battle/src/battle/helpers/makeBattleSave.ts
@@ -4,5 +4,5 @@ export const makeBattleSave = (battle: AlgolBattle): AlgolBattleSave => ({
   player: battle.gameEndedBy ? battle.winner! : battle.player,
   turn: battle.turnNumber,
   path: battle.path,
-  ended: !!battle.gameEndedBy,
+  ended: Boolean(battle.gameEndedBy),
 });
--- modules/local/src/session/stringifySession/stringifyBattleSave.ts
+++ modules/local/src/session/stringifySession/stringifyBattleSave.ts
@@ -4,7 +4,13 @@ import { stringifyPath } from "./stringifyPath";
 export const stringifyBattleSave = (save: AlgolBattleSave, method: number) => {
   const { path, player, turn, ended } = save;
   if (method === 0) {
-    return [method, player, ended, turn, stringifyPath(path, method)].join("|");
+    return [
+      method,
+      player,
+      ended ? "1" : "",
+      turn,
+      stringifyPath(path, method),
+    ].join("|");
   }
   throw new Error("Unknown stringification method");
 };

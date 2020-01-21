38e5e9e64fe21b18f60423ce1a586ceec3292d44
David Waller <dwaller@apple.com>
2020-01-21 08:55:54 +0100

    fix api export

--- modules/ui/src/components/Content/Content.stories.tsx
+++ modules/ui/src/components/Content/Content.stories.tsx
@@ -3,13 +3,13 @@ import { storiesOf } from "@storybook/react";
 import { select } from "@storybook/addon-knobs";
 
 import { Content } from ".";
-import allAPIs from "../../../../battle/dist/allAPIs";
+import allStatefulAPIs from "../../../../battle/dist/allStatefulAPIs";
 
 import { GameId, list } from "../../../../games/dist/list";
 
 storiesOf("Content", module).add("Battle start instructions", () => {
   const gameId = select("Game", list, list[0]) as GameId;
-  const api = allAPIs[gameId];
+  const api = allStatefulAPIs[gameId];
   const ui = api.newBattle().initialUI;
   return (
     <div>

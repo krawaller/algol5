b335dab0670ca9a824d81cdad4b2996fd90e4086
David Waller <dwaller@apple.com>
2020-01-24 12:33:09 +0100

    graphic template comment

--- modules/games/definitions/threemusketeers/graphics.ts
+++ modules/games/definitions/threemusketeers/graphics.ts
@@ -1,3 +1,8 @@
+// Here you define how terrains should be drawn, and what icons (pawn, rook, etc) to use
+// for the various groups. The group-icon mapping here is used as the source of truth for what
+// groups are available in your game. Add a group here, run the type analysis and it will be added
+// to the types for the game!
+
 import { ThreemusketeersGraphics } from "./_types";
 
 const threemusketeersGraphics: ThreemusketeersGraphics = {

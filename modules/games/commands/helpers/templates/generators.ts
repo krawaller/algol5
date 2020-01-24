1220a8f1a2ce8264bc5dc79504bebb4f554be4bc
David Waller <dwaller@apple.com>
2020-01-24 12:35:36 +0100

    generators template comment

--- modules/games/commands/helpers/templates/generators.ts
+++ modules/games/commands/helpers/templates/generators.ts
@@ -1,6 +1,11 @@
 export default function templateGenerators(gameId: string) {
   const capId = gameId[0].toUpperCase().concat(gameId.slice(1));
-  return `import { ${capId}Generators } from './_types';
+  return `// Here you define the generators (artifact creators) for your game. 
+// The type analyser will look at this file to see what generators are available,
+// and also what artifact layers there are. When you add/remove a generator or
+// change the names of the layers you draw to, rerun the type analyser!
+
+import { ${capId}Generators } from './_types';
 
 const ${gameId}Generators: ${capId}Generators = {
 
--- modules/games/definitions/murusgallicus/generators.ts
+++ modules/games/definitions/murusgallicus/generators.ts
@@ -1,3 +1,8 @@
+// Here you define the generators (artifact creators) for your game.
+// The type analyser will look at this file to see what generators are available,
+// and also what artifact layers there are. When you add/remove a generator or
+// change the names of the layers you draw to, rerun the type analyser!
+
 import { MurusgallicusGenerators } from "./_types";
 
 const murusgallicusGenerators: MurusgallicusGenerators = {

7f539b546b8255533d76c85a6c7b56f412aba537
David Waller <dwaller@apple.com>
2020-01-30 22:27:45 +0100

    export icons

--- modules/graphics/commands/exportIcons.ts
+++ modules/graphics/commands/exportIcons.ts
@@ -1,4 +1,5 @@
 import fs, { writeFileSync } from "fs-extra";
+import prettier from "prettier";
 import path from "path";
 import svgToMiniDataURI from "mini-svg-data-uri";
 import { icons } from "../picdata";
@@ -7,19 +8,50 @@ import { getIconShape } from "./helpers/getIconShape";
 const out = path.join(__dirname, "../dist");
 
 fs.emptyDirSync(path.join(out, "iconSVGs"));
+fs.emptyDirSync(path.join(out, "exportedIconSVGs"));
+
+const iconSVGs: Record<string, string> = {};
 
 for (const icon of icons) {
   for (const owner of [0, 1, 2] as (0 | 1 | 2)[]) {
     const def = getIconShape(icon, owner);
     const svg = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50">${def}</svg>`;
     writeFileSync(path.join(out, "iconSVGs", `${icon}${owner}.svg`), svg);
+    const exp = `export const ${icon}${owner}SVG = '${svg}';\n`;
+    writeFileSync(
+      path.join(out, "exportedIconSVGs", `${icon}${owner}SVG.ts`),
+      exp
+    );
+    iconSVGs[icon + owner] = svg;
   }
 }
 
-const collection = `${icons.map(icon => `export * from './${icon}'`).join("\n")}
-${icons.map(icon => `import { ${icon} } from './${icon}'`).join("\n")}
-export const allIcons = {
-${icons.map(icon => `${icon}`).join(", \n")}
-};`;
+const ownedIcons = icons
+  .map(icon => icon + "0")
+  .concat(icons.map(icon => icon + "1"))
+  .concat(icons.map(icon => icon + "2"));
 
-fs.writeFileSync(path.join(out, "allIconDefs.ts"), collection);
+const collection = `${ownedIcons
+  .map(icon => `export * from './exportedIconSVGs/${icon}SVG'`)
+  .join("\n")}
+${ownedIcons
+  .map(icon => `import { ${icon}SVG } from './exportedIconSVGs/${icon}SVG'`)
+  .join("\n")}
+export const allIcons = {
+${ownedIcons.map(icon => `${icon}SVG`).join(", ")}
+};`;
+fs.writeFileSync(
+  path.join(out, "allIconSVGs.ts"),
+  prettier.format(collection, { filepath: "foo.ts" })
+);
+
+const dataURIs = `export const iconDataURIs = {
+  ${ownedIcons
+    .map(icon => `${icon}: "${svgToMiniDataURI(iconSVGs[icon])}"`)
+    .join(", ")}
+};
+`;
+fs.writeFileSync(
+  path.join(out, "iconDataURIs.ts"),
+  prettier.format(dataURIs, { filepath: "foo.ts" })
+);

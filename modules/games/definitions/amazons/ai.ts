0b2f519ea21450e8992c37c5c3c7f77afa8fdb8d
David Waller <dwaller@apple.com>
2020-02-08 09:54:37 +0100

    amazons ai fix

--- modules/games/definitions/amazons/ai.ts
+++ modules/games/definitions/amazons/ai.ts
@@ -7,44 +7,44 @@ const amazonsAI: AmazonsAI = {
     findroads: {
       type: "neighbour",
       dirs: "rose",
-      starts: "queens",
+      starts: "amazons",
       unlessover: "units",
       draw: {
         start: {
           tolayer: {
-            ifelse: [{ anyat: ["myunits", ["start"]] }, "myroads", "opproads"]
+            ifelse: [{ anyat: ["myunits", ["start"]] }, "myroads", "opproads"],
           },
-          include: { count: ["neighbourcount"] }
-        }
-      }
+          include: { count: ["neighbourcount"] },
+        },
+      },
     },
     findreach: {
       type: "walker",
       dirs: "rose",
-      starts: "queens",
+      starts: "amazons",
       blocks: "units",
       draw: {
         steps: {
           tolayer: {
-            ifelse: [{ anyat: ["myunits", ["start"]] }, "myreach", "oppreach"]
-          }
-        }
-      }
-    }
+            ifelse: [{ anyat: ["myunits", ["start"]] }, "myreach", "oppreach"],
+          },
+        },
+      },
+    },
   },
   aspects: {
     myroads: { harvest: ["myroads", "count"] },
     mydomain: { sizeof: "myreach" },
     opproads: { harvest: ["opproads", "count"] },
-    oppdomain: { sizeof: "oppreach" }
+    oppdomain: { sizeof: "oppreach" },
   },
   brains: {
     Steve: {
       generators: ["findroads", "findreach"],
       plus: { myroads: 1, mydomain: 1 },
-      minus: { opproads: 1, oppdomain: 1 }
-    }
-  }
+      minus: { opproads: 1, oppdomain: 1 },
+    },
+  },
 };
 
 export default amazonsAI;

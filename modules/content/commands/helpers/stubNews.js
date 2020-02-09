3d43f3fdd96e231b6b6b63fd565f878b69625985
David Waller <dwaller@apple.com>
2020-02-09 11:05:25 +0100

    stubNews command

---
+++ modules/content/commands/helpers/stubNews.js
@@ -0,0 +1,29 @@
+const fs = require("fs-extra");
+const path = require("path");
+
+const stubNews = date => {
+  const out = path.join(__dirname, `../../material/news/${date}`);
+  if (fs.existsSync(out)) {
+    throw new Error(`News ${date} already exists!`);
+  } else if (!date || !date.match(/2[0-9]{3}-[0-9]{2}-[0-9]{2}/)) {
+    throw new Error(
+      `Illegal news date ${date}, should be formatted as yyyy-mm-dd`
+    );
+  } else {
+    fs.ensureDirSync(out);
+    fs.ensureDirSync(path.join(out, `pics`));
+    fs.writeFileSync(
+      path.join(out, `news.md`),
+      `---
+date: ${date}
+blurb: Add short description here!
+thumbnail: picToUseAsThumbnail.png
+---
+
+Omg you can NEVER guess what happened ${date}!! :D
+`
+    );
+  }
+};
+
+module.exports = stubNews;

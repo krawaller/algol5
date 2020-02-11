64a1f36d72a1f06a01a1a82b30988a25a1296268
David Waller <dwaller@apple.com>
2020-02-11 07:02:02 +0100

    handle outside position names

--- modules/common/positions/coords2pos.test.ts
+++ modules/common/positions/coords2pos.test.ts
@@ -5,7 +5,10 @@ test("coords2pos", () =>
     [{ x: 1, y: 1 }, "a1"],
     [{ x: 1, y: 3 }, "a3"],
     [{ x: 2, y: 4 }, "b4"],
-    [{ x: 10, y: 11 }, "j11"]
+    [{ x: 10, y: 11 }, "j11"],
+    [{ x: 0, y: 1 }, "Z1"],
+    [{ x: 3, y: 0 }, "c0"],
+    [{ x: 0, y: 0 }, "Z0"],
   ] as const).forEach(([coords, expectedPos]) =>
     expect(coords2pos(coords)).toBe(expectedPos)
   ));

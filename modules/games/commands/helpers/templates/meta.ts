7412880342091bf32db7e15fb266c6d6ad5e541c
David Waller <dwaller@apple.com>
2020-02-18 11:36:41 +0100

    autoassign new code to game stub

--- modules/games/commands/helpers/templates/meta.ts
+++ modules/games/commands/helpers/templates/meta.ts
@@ -1,3 +1,5 @@
+import { newCode } from "../newCode";
+
 export default function templateMeta(gameId: string) {
   const capId = gameId[0].toUpperCase().concat(gameId.slice(1));
   return `import { ${capId}Meta } from './_types';
@@ -8,7 +10,7 @@ const ${gameId}Meta: ${capId}Meta = {
   tags: [],
   tagline: "An awesome game!",
   source: "http://some.url.to.rules",
-  code: "" // this must be a unique character! try 'npm run checkCodes'
+  code: "${newCode()}"
 };
 
 export default ${gameId}Meta;
---
+++ modules/games/commands/unusedCode.ts
@@ -0,0 +1,5 @@
+import { newCode } from "./helpers/newCode";
+
+const c = newCode();
+
+console.log("Unused code:", c);

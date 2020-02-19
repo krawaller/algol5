86ece3a224fa85489d22741c4b342b2d034af824
David Waller <dwaller@apple.com>
2020-02-19 08:24:53 +0100

    add momentum test/demo

--- modules/games/definitions/momentum/scripts.ts
+++ modules/games/definitions/momentum/scripts.ts
@@ -1,5 +1,24 @@
 import { MomentumScripts } from "./_types";
 
-const momentumScripts: MomentumScripts = {};
+const momentumScripts: MomentumScripts = {
+  basic: [
+    {
+      commands: [
+        "a1",
+        "drop",
+        "endTurn",
+        "c3",
+        "drop",
+        "endTurn",
+        "d4",
+        "drop",
+        "endTurn",
+        "c3",
+        "drop",
+        "endTurn",
+      ],
+    },
+  ],
+};
 
 export default momentumScripts;

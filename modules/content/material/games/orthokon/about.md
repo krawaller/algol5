9c1fe640adffb7fa414e863260f80acd3e1d4b10
David Waller <dwaller@apple.com>
2020-02-21 07:30:36 +0100

    orthokon content

--- modules/content/material/games/orthokon/about.md
+++ modules/content/material/games/orthokon/about.md
@@ -1,1 +1,10 @@
-Orthokon! Take over your opponent's army before she steals yours. More yabber about the game to come!
+Orthokon was invented by L. Lynn Smith in 2001.
+
+{PIC:name=orthokon.png,cred=Photo by BGG user kkurianski,title=Plush Orthokon!}
+
+The simple rules where players take over each others pieces might seem to amount to a mindless back-and-forth going-on-forever game, but Orthokon will often reveal ways to surprise wins for the cunning!
+
+Links
+
+- {EXTLINK:text=BGG entry,url=https://www.boardgamegeek.com/boardgame/150586/descent}
+- {EXTLINK:text=World of Abstract Games entry,url=http://www.di.fc.ul.pt/~jpn/gv/orthokon.htm}
--- modules/content/material/games/orthokon/arrangements.ts
+++ modules/content/material/games/orthokon/arrangements.ts
@@ -9,7 +9,23 @@ import {
 // Add your arrangements in this object, and then you can refer to them
 // from the markdown files. Those references will be replaced by a
 // generated SVG when the content is written to html.
-export const arrangements: AlgolArrangements<
-  OrthokonPosition,
-  OrthokonUnit
-> = {};
+export const arrangements: AlgolArrangements<OrthokonPosition, OrthokonUnit> = {
+  beforemove1: {
+    marks: ["b1"],
+    potentialMarks: ["a1", "a2", "b4", "d3"],
+    setup: {
+      soldiers: {
+        1: ["c1", "c3", "c4", "d1", "d2"],
+        2: ["a4", "b1", "d4"],
+      },
+    },
+  },
+  aftermove1: {
+    setup: {
+      soldiers: {
+        1: ["c1", "c4", "d1"],
+        2: ["a4", "d2", "c3", "d3", "d4"],
+      },
+    },
+  },
+};

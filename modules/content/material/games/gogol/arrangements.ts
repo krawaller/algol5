732a5cc5dcc60f587856eeeb335a221e77b538e2
David Waller <dwaller@apple.com>
2020-02-25 07:40:46 +0100

    gogol content

--- modules/content/material/games/gogol/arrangements.ts
+++ modules/content/material/games/gogol/arrangements.ts
@@ -6,4 +6,35 @@ import { GogolPosition, GogolUnit } from "../../../../games/dist/games/gogol";
 // Add your arrangements in this object, and then you can refer to them
 // from the markdown files. Those references will be replaced by a
 // generated SVG when the content is written to html.
-export const arrangements: AlgolArrangements<GogolPosition, GogolUnit> = {};
+export const arrangements: AlgolArrangements<GogolPosition, GogolUnit> = {
+  homerow: {
+    setup: {
+      kings: {
+        1: ["e1"],
+      },
+      soldiers: {
+        1: ["d1"],
+      },
+    },
+  },
+  secondrow: {
+    setup: {
+      kings: {
+        1: ["e2"],
+      },
+      soldiers: {
+        1: ["d1", "e1", "f1"],
+      },
+    },
+  },
+  side: {
+    setup: {
+      kings: {
+        1: ["a4"],
+      },
+      soldiers: {
+        1: ["a3"],
+      },
+    },
+  },
+};
--- modules/content/material/games/gogol/links.ts
+++ modules/content/material/games/gogol/links.ts
@@ -1,1 +1,5 @@
-export const links = {};
+export const links = {
+  "Board Game Geek entry": "https://boardgamegeek.com/boardgame/3437/gogol",
+  "World of Abstract Games entry": "http://www.di.fc.ul.pt/~jpn/gv/gogol.htm",
+  "Gogol.nu flash website": "http://www.gogol.nu/",
+};

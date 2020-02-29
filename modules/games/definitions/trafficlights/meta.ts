a207bb539abfcbeed0344097e0f8bfdbed994c57
David Waller <dwaller@apple.com>
2020-02-29 15:32:47 +0100

    rename semaphor to traffic lights

--- modules/games/definitions/trafficlights/meta.ts
+++ modules/games/definitions/trafficlights/meta.ts
@@ -2,12 +2,12 @@ import { SemaphorMeta } from "./_types";
 
 const semaphorMeta: SemaphorMeta = {
   code: "F",
-  id: "semaphor",
-  name: "Semaphor",
+  id: "trafficlights",
+  name: "Traffic Lights",
   source: "http://www.di.fc.ul.pt/~jpn/gv/semaphor.htm",
   tags: ["commonunits", "formation", "deploy", "chooseaction", "changeunit"],
   tagline:
-    "Position and promote neutral units to form a line before your opponent"
+    "Position and promote neutral units to form a line before your opponent",
 };
 
 export default semaphorMeta;

e9cb58dadea5fe6967a89a9bb02715c55ba85d53
David Waller <dwaller@apple.com>
2020-02-29 16:07:24 +0100

    shoveoff content

--- modules/content/material/games/shoveoff/links.ts
+++ modules/content/material/games/shoveoff/links.ts
@@ -1,1 +1,5 @@
-export const links = {};
+export const links = {
+  "Geeky Hobbies entry":
+    "http://www.geekyhobbies.com/shove-off-board-game-review-and-rules/",
+  "Board Game Geek entry": "https://boardgamegeek.com/boardgame/20522/shove",
+};

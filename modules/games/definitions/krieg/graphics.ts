408154ef931bf1fc590ca68390fd84b9bf20e13d
David Waller <dwaller@apple.com>
2020-02-29 16:59:38 +0100

    krieg content

--- modules/games/definitions/krieg/graphics.ts
+++ modules/games/definitions/krieg/graphics.ts
@@ -7,7 +7,7 @@ import { KriegGraphics } from "./_types";
 
 const kriegGraphics: KriegGraphics = {
   tiles: { corners: "playercolour", bases: "castle" },
-  icons: { notfrozens: "knight", frozens: "knight" }
+  icons: { notfrozens: "knight", frozens: "rook" },
 };
 
 export default kriegGraphics;

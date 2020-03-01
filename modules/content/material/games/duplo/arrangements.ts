430ce52e9bd4f6d36863497c590cd77f1b129804
David Waller <dwaller@apple.com>
2020-03-01 13:02:51 +0100

    duplo content

--- modules/content/material/games/duplo/arrangements.ts
+++ modules/content/material/games/duplo/arrangements.ts
@@ -6,4 +6,29 @@ import { DuploPosition, DuploUnit } from "../../../../games/dist/games/duplo";
 // Add your arrangements in this object, and then you can refer to them
 // from the markdown files. Those references will be replaced by a
 // generated SVG when the content is written to html.
-export const arrangements: AlgolArrangements<DuploPosition, DuploUnit> = {};
+export const arrangements: AlgolArrangements<DuploPosition, DuploUnit> = {
+  before: {
+    setup: {
+      soldiers: {
+        1: ["d1", "e1", "f1"],
+        2: ["h1"],
+      },
+    },
+  },
+  left: {
+    setup: {
+      soldiers: {
+        1: ["a1", "b1", "c1", "d1", "e1", "f1"],
+        2: ["h1"],
+      },
+    },
+  },
+  right: {
+    setup: {
+      soldiers: {
+        0: ["h1"],
+        1: ["d1", "e1", "f1", "g1"],
+      },
+    },
+  },
+};

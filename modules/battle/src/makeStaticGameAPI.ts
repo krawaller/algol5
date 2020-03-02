d8bde87637f5f02735db5d9d6f908cff873628cc
David Waller <dwaller@apple.com>
2020-03-02 17:35:16 +0100

    make battleAPI take board book

--- modules/battle/src/makeStaticGameAPI.ts
+++ modules/battle/src/makeStaticGameAPI.ts
@@ -1,16 +1,25 @@
-import { AlgolGame, AlgolStaticGameAPI, AlgolSetupBookAnon } from "../../types";
+import {
+  AlgolGame,
+  AlgolStaticGameAPI,
+  AlgolSetupBookAnon,
+  AlgolBoardBookAnon,
+} from "../../types";
 import { newBattle, battleAction, getBattleUI } from "./battle";
 import { inflateBattleSave } from "./battle/helpers";
 
 export function makeStaticGameAPI(
   game: AlgolGame,
-  setups: AlgolSetupBookAnon
+  setups: AlgolSetupBookAnon,
+  boards: AlgolBoardBookAnon
 ): AlgolStaticGameAPI {
   return {
     gameId: game.gameId,
-    // TODO - variants choices selecting which setup
-    newBattle: () => newBattle(game, setups.basic),
-    fromSave: save => inflateBattleSave(game, save),
+    newBattle: () => {
+      // TODO - variants choices selecting which setup and board
+      game.setBoard(boards.basic);
+      return newBattle(game, setups.basic);
+    },
+    fromSave: save => inflateBattleSave(game, save), // TODO - read variant from save
     performAction: (battle, action, arg) =>
       battleAction(game, battle, action, arg),
     getBattleUI: battle => getBattleUI(game, battle),

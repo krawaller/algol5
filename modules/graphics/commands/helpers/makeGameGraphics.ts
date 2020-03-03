b597ed83b4b7ea37e16e5e9f2a978452f809cc28
David Waller <dwaller@apple.com>
2020-03-03 18:59:09 +0100

    make graphics module draw all boards

--- modules/graphics/commands/helpers/makeGameGraphics.ts
+++ modules/graphics/commands/helpers/makeGameGraphics.ts
@@ -3,29 +3,34 @@ import path from "path";
 import lib from "../../../games/dist/lib";
 import svgToMiniDataURI from "mini-svg-data-uri";
 import { render } from "../../render";
+import { AlgolGameGraphics } from "../../../types";
 
 const boardOut = path.join(__dirname, "../../dist/svgBoards");
 const jsonOut = path.join(__dirname, "../../dist/svgDataURIs");
 
 export async function makeGameGraphics(gameId: string) {
   const def = lib[gameId];
-  const { height, width } = def.board;
-
-  const boardSVG = render({ board: def.board, tileMap: def.graphics.tiles });
-
+  const graphics: AlgolGameGraphics = {
+    icons: def.graphics.icons,
+    boards: {},
+  };
   await fs.ensureDir(boardOut);
-  await fs.writeFile(path.join(boardOut, gameId + ".svg"), boardSVG);
+  for (const [name, boardDef] of Object.entries(def.boards)) {
+    const boardSVG = render({ board: boardDef, tileMap: def.graphics.tiles });
+    await fs.writeFile(path.join(boardOut, `${gameId}_${name}.svg`), boardSVG);
+    graphics.boards[name] = {
+      height: boardDef.height,
+      width: boardDef.width,
+      dataURI: svgToMiniDataURI(boardSVG),
+    };
+  }
+
   await fs.ensureDir(jsonOut);
   await fs.writeFile(
     path.join(jsonOut, gameId + ".ts"),
     `import { AlgolGameGraphics } from "../../../types";\n` +
       `export const ${gameId}BoardGraphics: AlgolGameGraphics = ${JSON.stringify(
-        {
-          height,
-          width,
-          icons: def.graphics.icons,
-          dataURI: svgToMiniDataURI(boardSVG),
-        },
+        graphics,
         null,
         2
       )};\n` +

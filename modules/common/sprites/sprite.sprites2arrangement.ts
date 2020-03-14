7ebc02891a51675df611c6c265d4d33b4cc106e8
David Waller <dwaller@apple.com>
2020-03-14 08:14:31 +0100

    finish sprites2arrangement and export

--- modules/common/sprites/sprite.sprites2arrangement.ts
+++ modules/common/sprites/sprite.sprites2arrangement.ts
@@ -1,8 +1,22 @@
-import { AlgolSprite, AlgolArrangement, AlgolGameBlobAnon } from "../../types";
+import {
+  AlgolSprite,
+  AlgolArrangement,
+  AlgolGameBlobAnon,
+  AlgolIconMap,
+} from "../../types";
+
+type Sprites2ArrangementOpts = {
+  sprites: AlgolSprite[];
+  iconMap: AlgolIconMap;
+};
 
 export const sprites2arrangement = (
-  sprites: AlgolSprite[]
+  opts: Sprites2ArrangementOpts
 ): AlgolArrangement<AlgolGameBlobAnon> => {
+  const { sprites, iconMap } = opts;
+  const revMap = Object.fromEntries(
+    Object.entries(iconMap).map(([group, icon]) => [icon, group])
+  );
   const ret: AlgolArrangement<AlgolGameBlobAnon> = {
     marks: [],
     potentialMarks: [],
@@ -11,16 +25,19 @@ export const sprites2arrangement = (
   for (const sprite of sprites) {
     const { mark, pos, unit } = sprite;
     if (mark === "mark") {
+      ret.marks = ret.marks || [];
       ret.marks.push(pos);
     }
     if (mark === "pot") {
+      ret.potentialMarks = ret.potentialMarks || [];
       ret.potentialMarks.push(pos);
     }
     if (unit) {
       const { icon, owner } = unit;
-      ret.setup[icon] = ret.setup[icon] || {};
-      ret.setup[icon][owner] = ret.setup[icon][owner] || [];
-      ret.setup[icon][owner].push(pos);
+      const group = revMap[icon];
+      ret.setup[group] = ret.setup[group] || {};
+      ret.setup[group]![owner] = ret.setup[group]![owner] || [];
+      ret.setup[group]![owner]!.push(pos);
     }
   }
   return ret;

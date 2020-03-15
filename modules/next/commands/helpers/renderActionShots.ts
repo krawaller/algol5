35d95d48f5c620307eed887cd1ef052e35b8ac93
David Waller <dwaller@apple.com>
2020-03-15 16:48:20 +0100

    use inlining of defs in action shots

--- modules/next/commands/helpers/renderActionShots.ts
+++ modules/next/commands/helpers/renderActionShots.ts
@@ -6,61 +6,41 @@ import { render } from "../../../graphics/render";
 import { arrangement2sprites } from "../../../common";
 
 const renderPNG = (obj: any) =>
-  new Promise((resolve, reject) => svgexport.render(obj, resolve));
+  new Promise(resolve => svgexport.render(obj, resolve));
 
 export const renderActionShots = async (gameId: string) => {
   const target = path.join(__dirname, `../../public/images/games/${gameId}`);
   fs.ensureDirSync(target);
   const def = lib[gameId];
-  for (const variant of def.variants) {
-    if (variant.arr) {
-      const board = def.boards[variant.board];
-      const picWithMarks = render({
-        board,
-        tileMap: def.graphics.tiles,
-        sprites: arrangement2sprites({
-          arrangement: variant.arr,
-          iconMap: def.graphics.icons,
-        }),
-      });
-      fs.writeFileSync(
+  //for (const variant of def.variants) {
+  const variant = def.variants[0];
+  if (variant.arr) {
+    const board = def.boards[variant.board];
+    const sprites = arrangement2sprites({
+      arrangement: variant.arr,
+      iconMap: def.graphics.icons,
+    });
+    const picWithMarks = render({
+      board,
+      tileMap: def.graphics.tiles,
+      sprites,
+      pad: true,
+      inline: true,
+    });
+    fs.writeFileSync(
+      path.join(target, `${gameId}_${variant.code}_active.svg`),
+      picWithMarks
+    );
+    await renderPNG({
+      input: [
         path.join(target, `${gameId}_${variant.code}_active.svg`),
-        picWithMarks
-      );
-      await renderPNG({
-        input: [
-          path.join(target, `${gameId}_${variant.code}_active.svg`),
-          "80%%",
-          "2x",
-        ],
-        output: [path.join(target, `${gameId}_${variant.code}_active.png`)],
-      });
-      const picWithoutMarks = render({
-        board,
-        tileMap: def.graphics.tiles,
-        sprites: arrangement2sprites({
-          arrangement: {
-            ...variant.arr,
-            marks: [],
-            potentialMarks: [],
-          },
-          iconMap: def.graphics.icons,
-        }),
-      });
-      fs.writeFileSync(
-        path.join(target, `${gameId}_${variant.code}.svg`),
-        picWithoutMarks
-      );
-      await renderPNG({
-        input: [
-          path.join(target, `${gameId}_${variant.code}.png`),
-          "80%%",
-          "2x",
-        ],
-        output: [path.join(target, `${gameId}_${variant.code}.png`)],
-      });
-    }
+        "100%",
+        "2x",
+      ],
+      output: [path.join(target, `${gameId}_${variant.code}_active.png`)],
+    });
   }
+  //}
 
   console.log("action shots rendered for", gameId);
 };

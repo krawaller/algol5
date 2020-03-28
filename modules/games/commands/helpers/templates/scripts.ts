67e92a606e087e1b61cd71ff0e692c1360f006aa
David Waller <dwaller@apple.com>
2020-03-28 10:36:45 +0100

    fix scripts stub

--- modules/games/commands/helpers/templates/scripts.ts
+++ modules/games/commands/helpers/templates/scripts.ts
@@ -2,9 +2,16 @@ export default function templateScripts(gameId: string) {
   const capId = gameId[0].toUpperCase().concat(gameId.slice(1));
   return `import { ${capId}Definition } from './_types';
 
-const ${gameId}Scripts: ${capId}Definition['scripts'] = {
-
-};
+const ${gameId}Scripts: ${capId}Definition['scripts'] = [
+  {
+    desc: "basic",
+    lines: [
+      {
+        commands: []
+      }
+    ]
+  }
+]
 
 export default ${gameId}Scripts;
 `;

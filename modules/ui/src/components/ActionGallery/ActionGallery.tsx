17d5b256a99a324e6ea4184f0bbee0b4a550bf5f
David Waller <dwaller@apple.com>
2020-03-28 16:48:36 +0100

    actiongallery component foundation

---
+++ modules/ui/src/components/ActionGallery/ActionGallery.tsx
@@ -0,0 +1,37 @@
+import React, { FunctionComponent } from "react";
+import { list } from "../../../../games/dist/list";
+import lib from "../../../../games/dist/lib";
+import meta from "../../../../games/dist/meta";
+import css from "./ActionGallery.cssProxy";
+
+type ActionGalleryProps = {
+  active?: boolean;
+};
+
+export const ActionGallery: FunctionComponent<ActionGalleryProps> = props => {
+  const { active } = props;
+  return (
+    <div className={css.actionGalleryContainer}>
+      {list.map(gameId => {
+        const variantCode = lib[gameId].variants[0].code;
+        const name = meta[gameId].name
+          .split(" ")
+          .map(word => <span key={word}>{word} </span>);
+        return (
+          <div className={css.actionGalleryItemBox}>
+            <img
+              key={gameId}
+              src={`/stolen/actionShots/${gameId}/${gameId}_${variantCode}${
+                active ? "_active" : ""
+              }.png`}
+            />
+            <div className={css.actionGalleryItemNameBox}>
+              <span className={css.actionGalleryItemNameInner}>{name}</span>
+            </div>
+          </div>
+        );
+      })}
+      <div className={css.actionGalleryEnding}>...and plenty more to come!</div>
+    </div>
+  );
+};

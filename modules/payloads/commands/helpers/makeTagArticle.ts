6c9e0bb369a388b0f175cccad6a5bc80d153e8fc
David Waller <dwaller@apple.com>
2020-04-15 17:15:24 +0200

    improve tagArticle generation

--- modules/payloads/commands/helpers/makeTagArticle.ts
+++ modules/payloads/commands/helpers/makeTagArticle.ts
@@ -2,33 +2,32 @@ import prettier from "prettier";
 import fs, { writeFileSync } from "fs-extra";
 import path from "path";
 import { getGamesForTag } from "./getGamesForTag";
-import { AlgolArticle, AlgolListing } from "../../../types";
 
 const tagArticlesFolder = path.join(__dirname, "../../dist/articles/tags");
 
 export const makeTagArticle = (tagId: string) => {
-  const gameListings = getGamesForTag(tagId).map(
-    gameId => require(`../../dist/listings/games/${gameId}`) as AlgolListing
-  );
+  const gameIds = getGamesForTag(tagId);
   const tagYaml = require(`../../../content/dist/tags/${tagId}/listing`)
     .listing;
   const tagHtml = require(`../../../content/dist/tags/${tagId}/tag`).tag;
-  const article: AlgolArticle = {
-    id: tagId,
-    blurb: tagYaml.blurb,
-    title: tagYaml.title,
-    mainImage: tagYaml.mainImage,
-    updated: tagYaml.updated,
-    relations: {
-      "Games with this tag": gameListings,
-    },
-    html: tagHtml,
-  };
   fs.ensureDirSync(tagArticlesFolder);
   const code = `
 // Created by the makeTagArticle command
+${gameIds
+  .map(gameId => `import ${gameId} from '../../listings/games/${gameId}'`)
+  .join("\n")}
 
-const article = ${JSON.stringify(article)};
+const article = {
+  id: "${tagId}",
+  blurb: "${tagYaml.blurb}",
+  title: "${tagYaml.title}",
+  mainImage: "${tagYaml.mainImage}",
+  updated: "${tagYaml.updated}",
+  relations: {
+    "Games with this tag": [${gameIds.join(", ")}]
+  },
+  html: \`${tagHtml}\`
+};
 
 export default article;
 `;

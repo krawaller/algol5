4b9dbc43e833efbeb0cb8814d6084de5327656e5
David Waller <dwaller@apple.com>
2020-04-18 09:21:47 +0200

    update stub commands

--- commands/stub.sh
+++ commands/stub.sh
@@ -3,33 +3,42 @@ gameId="$1"
 echo "Gonna stub ${gameId}"
 
 cd modules/games;
-npm run stub "$gameId"
-npm run analyze "$gameId"
-npm run export
+npm run stub "$gameId";
+npm run analyze "$gameId";
+npm run export;
 
 cd ../logic;
-npm run compile "$gameId"
-npm run generateGameTests
-npm run exportCompiledGames
+npm run compile "$gameId";
+npm run generateGameTests;
+npm run exportCompiledGames;
 
 cd ../graphics;
-npm run makeGameGraphics "$gameId"
-npm run exportGameGraphics
+npm run makeGameGraphics "$gameId";
+renderGameActionShots "$gameId";
+npm run exportGameGraphics;
 
 cd ../battle;
-npm run exportGameAPIs
-npm run generateGameTests
-npm run makeDemo "$gameId"
-npm run exportDemos
+npm run exportGameAPIs;
+npm run generateGameTests;
+npm run makeDemo "$gameId";
+npm run exportDemos;
 
 cd ../content;
-npm run stubGame "$gameId"
-npm run writeGame "$gameId"
+npm run stubGame "$gameId";
+npm run writeGame "$gameId";
 
 cd ../payloads;
-npm run makeGamePayloads "$gameId"
-npm run exportAllGamePayloads
+npm run makeGamePayload "$gameId";
+npm run exportAllGamePayloads;
+npm run makeGameListing "$gameId";
+npm run makeGameAboutArticle "$gameId";
+npm run makeGameRulesArticle "$gameId";
+npm run makeListingIndexes;
+npm run makeArticleIndexes;
+
+cd ../ui;
+npm run importGameImages;
 
 cd ../next;
-npm run makeGamePages
-npm run importGameImages
+npm run getStatic;
+npm run makeGamePages;

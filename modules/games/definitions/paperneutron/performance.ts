15077386686fd69a0d999e5d16a649decf6efa3e
David Waller <dwaller@apple.com>
2020-04-26 07:41:26 +0200

    delete paper neutron

--- modules/games/definitions/paperneutron/performance.ts
+++
@@ -1,9 +0,0 @@
-import { PaperneutronDefinition } from "./_types";
-
-const paperneutronPerformance: PaperneutronDefinition["performance"] = {
-  canAlwaysEnd: {},
-  massiveTree: {},
-  noEndGameCheck: []
-};
-
-export default paperneutronPerformance;

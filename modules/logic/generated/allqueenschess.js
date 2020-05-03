93e7717bb3420cbe800e328544865cb87e80a8f9
David Waller <dwaller@apple.com>
2020-05-03 08:41:28 +0200

    allqueenschess arr

--- modules/logic/generated/allqueenschess.js
+++ modules/logic/generated/allqueenschess.js
@@ -414,9 +414,14 @@ const game = {
       desc: "regular",
       code: "r",
       arr: {
-        setup: {},
-        marks: [],
-        potentialMarks: []
+        setup: {
+          queens: {
+            "1": ["a3", "b5", "c1", "c4", "d4", "d5"],
+            "2": ["b1", "c3", "c5", "d3", "e3"]
+          }
+        },
+        marks: ["b1"],
+        potentialMarks: ["a1", "a2", "c2", "b2", "b3", "b4"]
       }
     }
   ],

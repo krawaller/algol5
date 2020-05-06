d56f14e466e6f95e8120dd3ec924e0d0117014e6
David Waller <dwaller@apple.com>
2020-05-06 18:00:59 +0200

    add tag token

---
+++ modules/content/md2html/tokens/tag.ts
@@ -0,0 +1,25 @@
+import { TokenHandler } from "./_handler";
+import { intlink } from "./intlink";
+
+import tagIndex from "../../dist/tagIndex";
+
+// Takes a TAG token and turns it into clickable link
+
+export const tag: TokenHandler = opts => {
+  const { args } = opts;
+  let { id, text } = args;
+  if (!id) {
+    throw new Error("Tag ref didn't have id!");
+  }
+  const def = tagIndex[id];
+  if (!def) {
+    throw new Error(`Unknown tagId "${id}"!`);
+  }
+  return intlink({
+    ...opts,
+    args: {
+      text: text || def.title,
+      url: `/tags/${def.slug}`,
+    },
+  });
+};

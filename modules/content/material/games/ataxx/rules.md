745a1f89ff87d345e0793fa298cbba6224760478
David Waller <dwaller@apple.com>
2020-05-08 17:01:41 +0200

    ataxx content

--- modules/content/material/games/ataxx/rules.md
+++ modules/content/material/games/ataxx/rules.md
@@ -1,8 +1,16 @@
 ---
 updated: 2020-05-08
 ---
+
 {GAME} is played on a {DIM} board with the following setup:
 
 {SETUP}
 
-Soon we will show the rules for {GAME} here! In the meantime you can read them {EXTLINK:url=http://some.url.to.rules,text=here}.
+Players take turn to act with their {UNIT}, doing one of two things:
+
+- {CMND:name=jump} to an empty square two steps away (allowing diagonals)
+- {CMND:name=split} by spawning a new {UNIT} in an empty adjacent square
+
+In both cases, after acting, all enemy {UNIT} adjacent to the target square are taken over by the acting player.
+
+When the board is full, the player with the most {UNIT} wins!
--- modules/games/definitions/ataxx/scripts.ts
+++ modules/games/definitions/ataxx/scripts.ts
@@ -4,11 +4,21 @@ const ataxxScripts: AtaxxDefinition["scripts"] = [
   {
     desc: "basic",
     lines: [
-      {
-        commands: []
-      }
-    ]
-  }
+      { commands: ["a7", "b6", "split", "endTurn"] },
+      { commands: ["g7", "e5", "jump", "endTurn"] },
+      { commands: ["g1", "f2", "split", "endTurn"] },
+      { commands: ["e5", "f6", "split", "endTurn"] },
+      { commands: ["b6", "d6", "jump", "endTurn"] },
+      { commands: ["f6", "e6", "split", "endTurn"] },
+      { commands: ["f2", "e2", "split", "endTurn"] },
+      { commands: ["e5", "f3", "jump", "endTurn"] },
+      { commands: ["g1", "e3", "jump", "endTurn"] },
+      { commands: ["f6", "g4", "jump", "endTurn"] },
+      { commands: ["f2", "g3", "split", "endTurn"] },
+      { commands: ["d6", "f4", "jump", "endTurn"] },
+      { commands: ["f2", "e4", "jump", "endTurn"] },
+    ],
+  },
 ];
 
 export default ataxxScripts;

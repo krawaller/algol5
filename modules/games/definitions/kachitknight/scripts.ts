cf81d0dd1d9ae865b7deb580a334fdff5a73dc26
David Waller <dwaller@apple.com>
2020-05-18 07:08:11 +0200

    kachit action shot and demo

--- modules/games/definitions/kachitknight/scripts.ts
+++ modules/games/definitions/kachitknight/scripts.ts
@@ -4,11 +4,18 @@ const kachitknightScripts: KachitknightDefinition["scripts"] = [
   {
     desc: "basic",
     lines: [
-      {
-        commands: []
-      }
-    ]
-  }
+      { commands: ["c2", "diagonal", "endTurn"] },
+      { commands: ["b3", "diagonal", "endTurn"] },
+      { commands: ["d3", "orthogonal", "endTurn"] },
+      { commands: ["b4", "orthogonal", "endTurn"] },
+      { commands: ["d1", "d2", "step", "endTurn"] },
+      { commands: ["a2", "orthogonal", "endTurn"] },
+      { commands: ["d2", "c3", "orthogonal", "endTurn"] },
+      { commands: ["a2", "c2", "orthogonal", "endTurn"] },
+      { commands: ["c3", "b4", "diagonal", "endTurn"] },
+      { commands: ["a4", "b4", "step", "endTurn"] },
+    ],
+  },
 ];
 
 export default kachitknightScripts;
--- modules/games/definitions/kachitknight/variants.ts
+++ modules/games/definitions/kachitknight/variants.ts
@@ -8,11 +8,16 @@ const kachitknightVariants: KachitknightDefinition["variants"] = [
     desc: "regular",
     code: "r",
     arr: {
-      setup: {},
+      setup: {
+        leader: { 2: ["a4"] },
+        leaderortho: { 1: ["c3"] },
+        knightortho: { 1: ["d3"], 2: ["a2", "b4"] },
+        knightdiag: { 1: ["c2"], 2: ["b3"] },
+      },
       marks: [],
-      potentialMarks: []
-    }
-  }
+      potentialMarks: [],
+    },
+  },
 ];
 
 export default kachitknightVariants;

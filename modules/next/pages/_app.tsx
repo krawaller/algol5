c3b3098ae56817594e14ebdd80b14e70142717ea
David Waller <dwaller@apple.com>
2020-05-20 18:19:36 +0200

    use shell in next app component

--- modules/next/pages/_app.tsx
+++ modules/next/pages/_app.tsx
@@ -1,19 +1,22 @@
 import { AppProps } from "next/app";
+import { useMemo, useState } from "react";
+import { Shell } from "../../ui/src/components/Shell";
 import { appActions } from "../helpers/appActions";
-import { FunctionComponent, useMemo } from "react";
-import { useBattleNavActions } from "../helpers";
+import { useBattleNavActions, AlgolPage } from "../helpers";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const Comp = Component as FunctionComponent & { domain?: string };
+  const Comp = Component as AlgolPage;
   const [mode, sessionId, battleNavActions] = useBattleNavActions(Comp.domain);
-  const actions = useMemo(() => ({ ...appActions, ...battleNavActions }), [
-    battleNavActions,
-  ]);
+  const [nav, setNav] = useState();
+  const actions = useMemo(
+    () => ({ ...appActions, ...battleNavActions, setNav }),
+    [battleNavActions]
+  );
   const ctxt = useMemo(() => ({ sessionId, mode }), [sessionId, mode]);
   return (
-    <>
+    <Shell nav={nav}>
       <Component {...pageProps} actions={actions} ctxt={ctxt} />
-    </>
+    </Shell>
   );
 }
 

3408e46ddfa54a6faabd9885ac32da5deddf0ae4
David Waller <dwaller@apple.com>
2020-06-14 09:20:57 +0200

    clean up titlepage

--- modules/ui/src/components/TitlePage/TitlePage.tsx
+++ modules/ui/src/components/TitlePage/TitlePage.tsx
@@ -4,13 +4,11 @@
 
 import React, { Fragment } from "react";
 import { AlgolPage } from "../../../../types";
-import { list } from "../../../../games/dist/list";
 import { homeNav } from "../../../../common/nav/homeNav";
 import { Page } from "../Page";
 import base64CoffeePic from "../../../dist/base64/buymeacoffee3.png.proxy";
 import base64ForumPic from "../../../dist/base64/jointheforum3.png.proxy";
 import css from "./TitlePage.cssProxy";
-import { ButtonGroup } from "../ButtonGroup";
 import { Board } from "../Board";
 import { useBoard } from "./TitlePage.useBoard";
 import { setup2army } from "../../../../common";
@@ -19,11 +17,11 @@ export const TitlePage: AlgolPage = () => {
   const { graphics, setup, name } = useBoard();
   return (
     <Page
-      title="Welcome to Chessicals!"
+      title="Hello!"
       top={
         <Fragment>
           <div className={css.titlePageBlarb}>
-            <em>welcome to</em>
+            <em>Welcome to</em>
             <h3>Chessicals</h3>
             <em>where you can play</em>
             <h4>{name}</h4>
@@ -39,31 +37,22 @@ export const TitlePage: AlgolPage = () => {
         </Fragment>
       }
       body={
-        <Fragment>
-          <ButtonGroup>
-            <span>
-              A passion-powered collection of {list.length} abstract games for
-              you to enjoy! Very much beta software and not too many features,
-              but - welcome nonetheless!
-            </span>
-          </ButtonGroup>
-          <ButtonGroup>
-            <a
-              className={css.titlePageLink}
-              href="https://forum.chessicals.com"
-              target="_blank"
-              rel="noopener"
-              style={{ backgroundImage: `url(${base64ForumPic})` }}
-            />
-            <a
-              className={css.titlePageLink}
-              href="https://www.buymeacoffee.com/chessicals"
-              target="_blank"
-              rel="noopener"
-              style={{ backgroundImage: `url(${base64CoffeePic})` }}
-            />
-          </ButtonGroup>
-        </Fragment>
+        <div className={css.titlePageButtonContainer}>
+          <a
+            className={css.titlePageLink}
+            href="https://forum.chessicals.com"
+            target="_blank"
+            rel="noopener"
+            style={{ backgroundImage: `url(${base64ForumPic})` }}
+          />
+          <a
+            className={css.titlePageLink}
+            href="https://www.buymeacoffee.com/chessicals"
+            target="_blank"
+            rel="noopener"
+            style={{ backgroundImage: `url(${base64CoffeePic})` }}
+          />
+        </div>
       }
     />
   );

356b97b7d421d74d19865f4b2efde04787483b6c
David Waller <dwaller@apple.com>
2020-06-16 07:29:20 +0200

    add google analytics

--- modules/next/helpers/appActions.ts
+++ modules/next/helpers/appActions.ts
@@ -1,7 +1,18 @@
 import Router from "next/router";
 
+const ref = { last: "" };
+
 export const appActions = {
-  navTo: path => Router.push(path),
+  navTo: path => {
+    const global = (typeof window === "undefined" ? {} : window) as Window &
+      typeof globalThis & { ga: (a1: string, a2: string, a3?: string) => void };
+    Router.push(path);
+    if (global.ga && ref.last !== path) {
+      global.ga("set", "page", path);
+      global.ga("send", "pageview");
+      ref.last = path;
+    }
+  },
   replace: path => Router.replace(path),
   prefetch: path => {
     Router.prefetch(path);
--- modules/next/pages/_document.tsx
+++ modules/next/pages/_document.tsx
@@ -1,11 +1,24 @@
 import React from "react";
 import Document, { Head, Main, NextScript } from "next/document";
 
+const gaCode = `window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'UA-169617160-1');`;
+
+const gaScript = <script dangerouslySetInnerHTML={{ __html: gaCode }}></script>;
+
 export default class MyDocument extends Document {
   render() {
     return (
       <html>
         <Head>
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=UA-169617160-1"
+          ></script>
+          {gaScript}
           <link rel="stylesheet" href="/ui.css" />
           <meta property="og:site_name" content="Chessicals" />
           <meta name="apple-mobile-web-app-capable" content="yes" />

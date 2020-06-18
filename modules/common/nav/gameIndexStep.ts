aec58920364c4b4db2c364d3020eb7a4f1bc677d
David Waller <dwaller@apple.com>
2020-06-18 07:35:49 +0200

    fix count in game step

--- modules/common/nav/gameIndexStep.ts
+++ modules/common/nav/gameIndexStep.ts
@@ -1,10 +1,10 @@
 import { AlgolNavStep } from "../../types";
-import { list } from "../../games/dist/list";
 import { tagIndexStep } from "./tagIndexStep";
+import { gameCount } from "../utils";
 
 export const gameIndexStep: AlgolNavStep = {
   title: "Games",
-  desc: `List of all ${list.length} games`,
+  desc: `List of all ${gameCount()} games`,
   url: "/games",
   links: [tagIndexStep],
 };

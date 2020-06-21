c92095e67108fa18ad46c1ecad49563894e0b1b6
David Waller <dwaller@apple.com>
2020-06-21 14:59:37 +0200

    hobbes actionshot and demo

--- modules/games/definitions/hobbes/scripts.ts
+++ modules/games/definitions/hobbes/scripts.ts
@@ -4,11 +4,13 @@ const hobbesScripts: HobbesDefinition["scripts"] = [
   {
     desc: "basic",
     lines: [
-      {
-        commands: []
-      }
-    ]
-  }
+      { commands: ["d1", "e1", "push", "endTurn"] },
+      { commands: ["c4", "c3", "push", "endTurn"] },
+      { commands: ["d2", "d3", "push", "endTurn"] },
+      { commands: ["b4", "a4", "push", "endTurn"] },
+      { commands: ["e4", "move", "d4", "c4", "push", "endTurn"] },
+    ],
+  },
 ];
 
 export default hobbesScripts;
--- modules/games/definitions/hobbes/variants.ts
+++ modules/games/definitions/hobbes/variants.ts
@@ -8,11 +8,19 @@ const hobbesVariants: HobbesDefinition["variants"] = [
     desc: "regular",
     code: "r",
     arr: {
-      setup: {},
+      setup: {
+        kings: {
+          1: ["d2"],
+          2: ["b4"],
+        },
+        stones: {
+          0: ["a4", "b1", "b2", "b5", "c2", "c3", "d3", "d4", "d5", "e1"],
+        },
+      },
       marks: [],
-      potentialMarks: []
-    }
-  }
+      potentialMarks: ["c1", "c2", "d1", "d3", "e2", "e4", "e5"],
+    },
+  },
 ];
 
 export default hobbesVariants;

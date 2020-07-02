57ef4c8b17e70e855870771e885e33294a90083e
David Waller <dwaller@apple.com>
2020-07-02 13:08:56 +0200

    action shot and demo

--- modules/games/definitions/tunneler/scripts.ts
+++ modules/games/definitions/tunneler/scripts.ts
@@ -4,11 +4,12 @@ const tunnelerScripts: TunnelerDefinition["scripts"] = [
   {
     desc: "basic",
     lines: [
-      {
-        commands: []
-      }
-    ]
-  }
+      { commands: ["i2", "i3", "move", "endTurn"] },
+      { commands: ["g10", "g9", "move", "endTurn"] },
+      { commands: ["g4", "h4", "move", "endTurn"] },
+      { commands: ["i10", "h10", "move", "endTurn"] },
+    ],
+  },
 ];
 
 export default tunnelerScripts;

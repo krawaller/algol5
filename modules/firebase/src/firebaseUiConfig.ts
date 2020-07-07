0df3bbbe6d65c5d5ebe40865cf946c5bc0b4929e
David Waller <dwaller@apple.com>
2020-07-07 14:59:40 +0200

    missing peer dep

--- modules/firebase/src/firebaseUiConfig.ts
+++ modules/firebase/src/firebaseUiConfig.ts
@@ -2,7 +2,12 @@ import firebase from "firebase";
 import * as firebaseui from "firebaseui";
 
 export const firebaseUiConfig: firebaseui.auth.Config = {
-  signInOptions: [firebase.auth.EmailAuthProvider.PROVIDER_ID],
+  signInOptions: [
+    firebase.auth.EmailAuthProvider.PROVIDER_ID,
+    //firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+    firebase.auth.GithubAuthProvider.PROVIDER_ID,
+  ],
+  signInFlow: "popup",
   callbacks: {
     signInSuccessWithAuthResult: (result: any) => {
       return false; // To prevent redirect

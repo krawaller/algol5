092baac99754e2082e768d3c6ad3bf70b288a4d2
David <david@krawaller.se>
2020-07-11 07:02:37 +0200

    finish desdemona

--- modules/games/definitions/desdemona/meta.ts
+++ modules/games/definitions/desdemona/meta.ts
@@ -4,7 +4,7 @@ const desdemonaMeta: DesdemonaDefinition["meta"] = {
   id: "desdemona",
   name: "Desdemona",
   author: "Rey Alicea",
-  tags: [],
+  tags: ["irreversible"],
   tagline: "An Amazons-Othello lovechild",
   source: "http://some.url.to.rules",
   code: "g",

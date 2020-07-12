6c55b41fa8797596cad00c455aa99cf6157346a6
David <david@krawaller.se>
2020-07-12 09:34:39 +0200

    connect purge to actiondef

--- modules/types/gamedef/flow/actions/action.ts
+++ modules/types/gamedef/flow/actions/action.ts
@@ -1,12 +1,14 @@
 import { AlgolGenRef, AlgolLink } from "../../../";
 import { AlgolEffect } from "../../../";
 import { AlgolGameBlobAnon } from "../../../blob";
+import { AlgolArtifactLayerRef } from "../../../primitives/artifactlayerref";
 
 export type AlgolActionDef<Blob extends AlgolGameBlobAnon> = {
   link?: AlgolLink<Blob>;
   links?: AlgolLink<Blob>[];
   runGenerator?: AlgolGenRef<Blob>;
   runGenerators?: AlgolGenRef<Blob>[];
+  purge?: AlgolArtifactLayerRef<Blob>[];
 };
 
 export type AlgolEffectActionDef<

46098d31de00f5a9362c0970ff0d9fd5c7e299e4
David <david@krawaller.se>
2020-07-12 09:34:02 +0200

    artifactlayer ref

---
+++ modules/types/primitives/artifactlayerref/artifactlayerref.anon.ts
@@ -0,0 +1,10 @@
+import { AlgolArtifactLayerRef, AlgolArtifactLayerRefInner } from "./";
+import { AlgolGameBlobAnon } from "../../blob";
+
+export type AlgolArtifactLayerRefAnon = AlgolArtifactLayerRef<
+  AlgolGameBlobAnon
+>;
+
+export type AlgolArtifactLayerRefInnerAnon = AlgolArtifactLayerRefInner<
+  AlgolGameBlobAnon
+>;

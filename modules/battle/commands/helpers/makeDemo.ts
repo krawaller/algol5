797691bdbbe645bcdf153c08a949a19f68f5387d
David <david@krawaller.se>
2020-07-24 20:43:07 +0200

    make demo from seed

--- modules/battle/commands/helpers/makeDemo.ts
+++ modules/battle/commands/helpers/makeDemo.ts
@@ -1,21 +1,34 @@
-import { makeStatefulGameAPI } from "../../src";
+import { makeStatefulGameAPI, makeStaticGameAPI } from "../../src";
 import gameDefs from "../../../games/dist/lib";
 import { GameId } from "../../../games/dist/list";
 import games from "../../../logic/dist";
-import { AlgolScriptLine, AlgolGameBlobAnon } from "../../../types";
+import { AlgolScriptLine, AlgolGameBlobAnon, AlgolDemo } from "../../../types";
 import * as fs from "fs-extra";
 import * as path from "path";
 
-import { makeDemo as demoMaker } from "../../../common";
+import { makeDemoFromScript } from "../../../common";
+import { parseSeed } from "../../../encoding/src/seed/seed.parse";
+import { makeDemoFromBattle } from "../../../common/demo/makeDemoFromBattle";
 
 const out = path.join(__dirname, "../../dist/demos");
 
 export async function makeDemo(gameId: GameId) {
   const gameDef = gameDefs[gameId];
-  const scripts = gameDef.scripts;
-  const script: AlgolScriptLine<AlgolGameBlobAnon>[] = scripts[0].lines;
-  const API = makeStatefulGameAPI(games[gameId]);
-  const { anims, initial, patches } = demoMaker(API, script);
+  const game = games[gameId];
+  let demo: AlgolDemo;
+  if (gameDef.meta.demo) {
+    const API = makeStaticGameAPI(game);
+    const save = parseSeed(gameDef.meta.demo, gameId);
+    const battle = API.fromSave(save);
+    demo = makeDemoFromBattle(battle);
+  } else {
+    const scripts = gameDef.scripts;
+    const script: AlgolScriptLine<AlgolGameBlobAnon>[] = scripts[0].lines;
+    const API = makeStatefulGameAPI(game);
+    demo = makeDemoFromScript(API, script);
+  }
+
+  const { anims, initial, patches } = demo;
 
   await fs.ensureDir(out);
 

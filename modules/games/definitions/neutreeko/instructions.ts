01a9c7975152b1ed1b1055e4bbc9a486e6429787
Lucas Wiener <lucas@wiener.se>
2020-09-22 11:24:10 +0200

    Update modules/games/definitions/neutreeko/instructions.ts
    Co-authored-by: David Waller <info@krawaller.se>

--- modules/games/definitions/neutreeko/instructions.ts
+++ modules/games/definitions/neutreeko/instructions.ts
@@ -1,7 +1,7 @@
 import { NeutreekoDefinition } from "./_types";
 
 const neutreekoInstructions: NeutreekoDefinition["instructions"] = {
-  startTurn: { line: ["Select", { unittype: ["pieces", ["player"]]}, "to move"]},
+  startTurn: { line: ["Select", "pieces", "to move"]},
   selectunit: { line: ["Select", "where to move", { unitat: "selectunit" }] },
   selectmovetarget: {
     line: [

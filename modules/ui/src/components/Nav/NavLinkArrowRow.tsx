2b26050f6826d894f909a83250a94c98b89bd59c
David <david@krawaller.se>
2020-09-25 17:36:42 +0200

    clean up arrowrow render logic

--- modules/ui/src/components/Nav/NavLinkArrowRow.tsx
+++ modules/ui/src/components/Nav/NavLinkArrowRow.tsx
@@ -13,42 +13,22 @@ type NavLinkArrowRowProps = {
 
 export const NavLinkArrowRow: FunctionComponent<NavLinkArrowRowProps> = props => {
   const { nbrOfLinks, hasBackBtn, hasShortcut } = props;
-  const pieceCount = nbrOfLinks ? nbrOfLinks * 2 + (hasBackBtn ? 3 : 1) : 0;
+  const buttonCount = nbrOfLinks + (hasBackBtn ? 1 : 0);
+  const pieceCount = buttonCount * 2 - 1;
   const middle = Math.ceil(pieceCount / 2);
   const pieces = [];
   for (let i = 1; i <= pieceCount; i++) {
+    const side = i < middle ? "left" : i > middle ? "right" : "middle";
     if (i % 2) {
-      // filler
       pieces.push(
-        <div key={i} className={navCss.navFiller}>
-          {i === middle ? (
-            <ArrowMulti
-              {...middleArrows(nbrOfLinks, hasBackBtn, hasShortcut)}
-            />
-          ) : i > 1 && i < pieceCount && !(i < middle && hasBackBtn) ? (
-            <Arrow layout="eastwest" />
-          ) : null}
+        <div key={i} className={navCss.navButton}>
+          {aboveButtonArrows(side, buttonCount, hasBackBtn, hasShortcut)}
         </div>
       );
     } else {
-      // above button
       pieces.push(
-        <div key={i} className={navCss.navButton}>
-          {i < middle ? (
-            hasBackBtn ? null : i === 2 ? (
-              <Arrow layout="southeast" head="south" />
-            ) : (
-              <ArrowMulti southeast eastwest head="south" />
-            )
-          ) : i === middle ? (
-            <ArrowMulti
-              {...middleArrows(nbrOfLinks, hasBackBtn, hasShortcut)}
-            />
-          ) : i === pieceCount - 1 ? (
-            <Arrow layout="southwest" head="south" />
-          ) : (
-            <ArrowMulti southwest eastwest head="south" />
-          )}
+        <div key={i} className={navCss.navFiller}>
+          {spaceArrows(side, buttonCount, hasBackBtn, hasShortcut)}
         </div>
       );
     }
@@ -63,24 +43,48 @@ export const NavLinkArrowRow: FunctionComponent<NavLinkArrowRowProps> = props =>
       >
         {hasBackBtn && <Arrow layout="northsouth" />}
       </div>
+      <div className={navCss.navFiller} />
       {pieces}
+      <div className={navCss.navFiller} />
       <div className={navCss.navSideButtonContainer}></div>
     </div>
   );
 };
 
-const middleArrows = (
-  nbrOfLinks: number,
+const spaceArrows = (
+  side: "left" | "middle" | "right",
+  buttonCount: number,
   hasBackBtn?: boolean,
   hasShortcut?: boolean
-): ArrowMultiProps => {
-  const count = nbrOfLinks + (hasBackBtn ? 1 : 0);
-  const odd = Boolean(count % 2);
-  const result: ArrowMultiProps = {
-    head: odd ? "south" : undefined,
-    northsouth: odd,
-    northeast: count > 1,
-    northwest: count > 1 && !hasBackBtn,
-  };
-  return result;
-};
+) => (
+  <ArrowMulti
+    northwest={side === "middle" && buttonCount > 1 && !hasBackBtn}
+    northeast={side === "middle" && buttonCount > 1 && !hasShortcut}
+    eastwest={
+      (side === "left" && buttonCount > 1 && !hasBackBtn) ||
+      (side === "right" && buttonCount > 1 && !hasShortcut)
+    }
+  />
+);
+
+const aboveButtonArrows = (
+  side: "left" | "middle" | "right",
+  buttonCount: number,
+  hasBackBtn?: boolean,
+  hasShortcut?: boolean
+) => (
+  <ArrowMulti
+    head={
+      (side === "middle" && (!hasBackBtn || buttonCount > 1)) ||
+      (side === "right" && !hasShortcut) ||
+      (side === "left" && !hasBackBtn)
+        ? "south"
+        : undefined
+    }
+    northsouth={side === "middle" && (!hasBackBtn || buttonCount > 1)}
+    northeast={side === "middle" && buttonCount > 1 && !hasShortcut}
+    northwest={side === "middle" && buttonCount > 1 && !hasBackBtn}
+    southeast={side === "left" && !hasBackBtn}
+    southwest={side === "right" && !hasShortcut}
+  />
+);

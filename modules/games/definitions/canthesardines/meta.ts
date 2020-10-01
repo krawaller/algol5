97990e2b6eaa00b727d1cdc1ff1bb7352b728672
David Waller <dwaller@apple.com>
2020-10-01 18:17:49 +0200

    add can the sardines

---
+++ modules/games/definitions/canthesardines/meta.ts
@@ -0,0 +1,14 @@
+import { CanthesardinesDefinition } from "./_types";
+
+const canthesardinesMeta: CanthesardinesDefinition["meta"] = {
+  id: "canthesardines",
+  author: "Fred Horn",
+  name: "Can the Sardines",
+  tags: ["infiltration"],
+  tagline: "Can you can first?",
+  source: "http://www.di.fc.ul.pt/~jpn/gv/can_sardines.htm",
+  code: "k8",
+  slug: "canthesardines",
+};
+
+export default canthesardinesMeta;

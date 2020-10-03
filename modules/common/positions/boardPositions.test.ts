6d0719bcde6c51e47e8b1a23cbe81b7473c7fcde
David <david@krawaller.se>
2020-10-03 07:57:08 +0200

    exclude holes in boardPositions

--- modules/common/positions/boardPositions.test.ts
+++ modules/common/positions/boardPositions.test.ts
@@ -1,9 +1,20 @@
 import { boardPositions } from "..";
 import { AlgolBoardAnon } from "../../types";
-
-test("boardPositions", () => {
-  const board = ({ height: 2, width: 3 } as unknown) as AlgolBoardAnon;
-  const result = boardPositions(board);
-  const expected = ["a1", "a2", "b1", "b2", "c1", "c2"];
-  expect(result).toEqual(expected);
+describe("boardPositions", () => {
+  test("normal board", () => {
+    const board = ({ height: 2, width: 3 } as unknown) as AlgolBoardAnon;
+    const result = boardPositions(board);
+    const expected = ["a1", "a2", "b1", "b2", "c1", "c2"];
+    expect(result).toEqual(expected);
+  });
+  test("board with holes", () => {
+    const board: AlgolBoardAnon = {
+      height: 3,
+      width: 3,
+      holes: [{ rect: ["a1", "b2"] }, "c3"],
+    };
+    const result = boardPositions(board);
+    const expected = ["a3", "b3", "c1", "c2"];
+    expect(result).toEqual(expected);
+  });
 });
--- modules/common/positions/boardPositions.ts
+++ modules/common/positions/boardPositions.ts
@@ -1,12 +1,17 @@
 import { coords2pos } from "../";
 import { AlgolBoardAnon } from "../../types";
+import { processEntity } from "../entities";
 
 export function boardPositions(board: AlgolBoardAnon) {
   const { height, width } = board;
-  let ret = [];
+  const ret = [];
+  const holes = (board.holes || []).flatMap(processEntity).map(coords2pos);
   for (let y = 1; y <= height; y++) {
     for (let x = 1; x <= width; x++) {
-      ret.push(coords2pos({ x: x, y: y }));
+      const pos = coords2pos({ x: x, y: y });
+      if (!holes.includes(pos)) {
+        ret.push(pos);
+      }
     }
   }
   return ret.sort();

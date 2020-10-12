f0f580af0e377f09ededd515b570d47d140e457f
David <david@krawaller.se>
2020-10-12 21:33:10 +0200

    update demo

--- modules/games/definitions/desdemona/meta.ts
+++ modules/games/definitions/desdemona/meta.ts
@@ -11,6 +11,8 @@ const desdemonaMeta: DesdemonaDefinition["meta"] = {
   slug: "desdemona",
   hidden: false,
   added: "2020-07-11",
+  demo:
+    "g0Gn15|1ZTK8,8if9,6:!8f8Ldd3ha2aLd4RraTmRd1c1az2bvaa6o5n6Bb1aracgb1beic3e1bg1cmbg",
 };
 
 export default desdemonaMeta;

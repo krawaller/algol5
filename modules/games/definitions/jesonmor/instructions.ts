dd78a3c54f16e9ac5ed50913f3f8f6a811a81ed2
David <david@krawaller.se>
2020-11-16 19:05:05 +0100

    add centerkill hint to instruction

--- modules/games/definitions/jesonmor/instructions.ts
+++ modules/games/definitions/jesonmor/instructions.ts
@@ -1,7 +1,21 @@
 import { JesonmorDefinition } from "./_types";
 
 const jesonmorInstructions: JesonmorDefinition["instructions"] = {
-  startTurn: { line: ["Select", "which", "horses", "to act with"] },
+  startTurn: {
+    ifelse: [
+      { anyat: ["units", { onlyin: "center" }] },
+      {
+        line: [
+          "Select",
+          "a",
+          "horses",
+          "to oust",
+          { unitat: { onlyin: "center" } },
+        ],
+      },
+      { line: ["Select", "which", "horses", "to act with"] },
+    ],
+  },
   selectunit: { line: ["Select", "where to move", { unitat: "selectunit" }] },
   selectmovetarget: {
     line: [
@@ -13,11 +27,11 @@ const jesonmorInstructions: JesonmorDefinition["instructions"] = {
         ifelse: [
           { anyat: ["units", "selectmovetarget"] },
           { line: ["stomp", { unitat: "selectmovetarget" }] },
-          { line: ["go to", "selectmovetarget"] }  
-        ]
-      }
+          { line: ["go to", "selectmovetarget"] },
+        ],
+      },
     ],
-  }
+  },
 };
 
 export default jesonmorInstructions;

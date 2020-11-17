60d7f98de9e2c716b5b64b39bc3ba2daf44bd327
David <david@krawaller.se>
2020-11-17 06:37:49 +0100

    link data instead of copy in title payload

--- modules/payloads/commands/makeTitlePayload.ts
+++ modules/payloads/commands/makeTitlePayload.ts
@@ -8,6 +8,28 @@ import graphics from "../../graphics/dist/svgDataURIs";
 const out = path.join(__dirname, "../dist");
 fs.ensureDirSync(out);
 
+const gameIds = list.filter(gameId => !defs[gameId].meta.hidden);
+
+const imports = gameIds
+  .map(
+    gameId => `import ${gameId}Graphics from '../../graphics/dist/svgDataURIs/${gameId}'
+import ${gameId}Meta from '../../games/definitions/${gameId}/meta'
+import ${gameId}Variants from '../../games/definitions/${gameId}/variants'`
+  )
+  .join("\n");
+
+const items = gameIds
+  .map(
+    gameId => `  {
+    gameId: "${gameId}",
+    slug: ${gameId}Meta.slug,
+    name: ${gameId}Meta.name,
+    setup: ${gameId}Variants[0].arr!.setup,
+    graphics: ${gameId}Graphics
+  },`
+  )
+  .join("\n");
+
 const data = list
   .filter(gameId => !defs[gameId].meta.hidden)
   .map(gameId => ({
@@ -20,6 +42,7 @@ const data = list
 
 const code = prettier.format(
   `import { AlgolSetupAnon, AlgolGameGraphics } from '../../types'
+${imports}
 
 export type TitleData = {
   gameId: string
@@ -29,7 +52,9 @@ export type TitleData = {
   graphics: AlgolGameGraphics
 }
   
-export const data: TitleData[] = ${JSON.stringify(data)}; export default data`,
+export const data: TitleData[] = [
+${items}
+]; export default data`,
   { filepath: "foo.ts" }
 );
 

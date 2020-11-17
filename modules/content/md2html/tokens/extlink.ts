d66441e84839b854485728b0bebc66f79602291d
David <david@krawaller.se>
2020-11-17 03:20:44 +0100

    support to show links as buttons

--- modules/content/md2html/tokens/extlink.ts
+++ modules/content/md2html/tokens/extlink.ts
@@ -4,15 +4,17 @@ import { TokenHandler } from "./_handler";
 
 export const extlink: TokenHandler = opts => {
   const { args } = opts;
-  const { url, text } = args;
+  const { url, text, style } = args;
   if (!url) {
     throw new Error("Have to provide extlink url!");
   }
   if (!text) {
     throw new Error("Have to provide extlink text!");
   }
-  return `<a class="md-external-link" href="${url.replace(
-    /EQUALS/g,
-    "="
-  )}" target="_blank" rel="noopener">${text}</a>`;
+  const processedText = url.replace(/EQUALS/g, "=").replace(/COLON/g, ":");
+  const link = `<a class="md-external-link" href="${processedText}" target="_blank" rel="noopener">${text}</a>`;
+  if (style === "button") {
+    return `<button class="button">${link}</button>`;
+  }
+  return link;
 };

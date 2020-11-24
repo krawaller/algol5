a7fa493dbc71b4c2c1e68fcf724b0ebb11f733aa
David <david@krawaller.se>
2020-11-24 20:16:06 +0100

    don't SSR title page

--- modules/next/pages/index.tsx
+++ modules/next/pages/index.tsx
@@ -1,3 +1,13 @@
-import { TitlePage } from "../../ui/src/components/TitlePage";
+import dynamic from "next/dynamic";
+import { AlgolPageProps } from "../../types";
 
-export default TitlePage;
+// We don't want to server-side-render the TitlePage,
+// since it will have different content depending on
+// local data. This would case a server-client mismatch.
+
+const LazyTitlePage = dynamic(
+  () => import("../../ui/src/components/TitlePage/TitlePage"),
+  { ssr: false }
+);
+
+export default (props: AlgolPageProps) => <LazyTitlePage {...props} />;

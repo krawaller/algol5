b1d1cdf9005aadab8dddd909505f1764584d35a9
David <david@krawaller.se>
2020-12-04 14:29:53 +0100

    more big board support fixes

--- modules/common/positions/pos2coords.test.ts
+++ modules/common/positions/pos2coords.test.ts
@@ -6,6 +6,8 @@ test("pos2coords", () =>
     ["a3", { x: 1, y: 3 }],
     ["b4", { x: 2, y: 4 }],
     ["j11", { x: 10, y: 11 }],
+    ["k12", { x: 11, y: 12 }],
+    ["l12", { x: 12, y: 12 }],
     ["Z0", { x: 0, y: 0 }],
   ] as const).forEach(([pos, expectedCoords]) =>
     expect(pos2coords(pos)).toEqual(expectedCoords)

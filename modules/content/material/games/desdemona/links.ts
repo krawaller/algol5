4921c39939f83ee323d3bbe43e03007069bf63a8
David <david@krawaller.se>
2020-12-09 18:28:36 +0100

    add links to reys blog

--- modules/content/material/games/desdemona/links.ts
+++ modules/content/material/games/desdemona/links.ts
@@ -1,4 +1,6 @@
 export const links = {
+  "Ludo Soup Games entry":
+    "https://reyaliceagames.wordpress.com/2020/09/14/desdemona/",
   "Mindsports entry": "https://mindsports.nl/index.php/the-pit/1046-desdemona",
   "Board Game Geek entry":
     "https://boardgamegeek.com/boardgame/313894/desdemona",
--- modules/content/material/games/sapos/links.ts
+++ modules/content/material/games/sapos/links.ts
@@ -1,3 +1,5 @@
 export const links = {
+  "Ludo Soup Games entry":
+    "https://reyaliceagames.wordpress.com/2017/01/28/sapos/",
   "Board Game Geek entry": "https://boardgamegeek.com/boardgame/132411/sapos",
 };

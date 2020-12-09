902a98ebe89b843dcd7cc1aa10f3b0b061523aa0
David <david@krawaller.se>
2020-12-09 07:57:29 +0100

    update lint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,8 @@
 module.exports = {
+  env: {
+    browser: true,
+    node: true,
+  },
   root: true,
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint"],
@@ -6,5 +10,17 @@ module.exports = {
   rules: {
     "@typescript-eslint/explicit-module-boundary-types": "off",
     "@typescript-eslint/no-non-null-assertion": "off",
+    "@typescript-eslint/no-empty-function": "off",
+    // TODO - kill this?
+    "@typescript-eslint/no-explicit-any": "off",
   },
+  overrides: [
+    {
+      files: ["ai.ts", "_types.ts"],
+      rules: {
+        "@typescript-eslint/no-unused-vars": "off",
+        "@typescript-eslint/no-explicit-any": "off",
+      },
+    },
+  ],
 };

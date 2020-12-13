096a4fe609228b793bfe1d05144ceea18122ce1e
Joel Fransson <joel@greenfield.se>
2020-12-13 20:34:30 +0100

    Update modules/games/definitions/outwit/instructions.ts
    Co-authored-by: David Waller <david@krawaller.se>

--- modules/games/definitions/outwit/instructions.ts
+++ modules/games/definitions/outwit/instructions.ts
@@ -1,7 +1,15 @@
 import { OutwitDefinition } from "./_types";
 
 const outwitInstructions: OutwitDefinition["instructions"] = {
-  startTurn: { line: ["Select a piece to move"] },
+  startTurn: {
+    line: [
+      "Select a"
+      "soldiers",
+      "or",
+      "kings",
+      "to move"
+    ]
+  },
   selectunit: { line: ["Select where to move", { unitat: "selectunit" }] },
   selectmovetarget: {
     line: [

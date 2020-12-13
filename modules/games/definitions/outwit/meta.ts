f8c3def21d324f4c5fa686778f19bb8dcc517089
Joel Fransson <joel@greenfield.se>
2020-12-13 17:16:33 +0100

    Update meta

--- modules/games/definitions/outwit/meta.ts
+++ modules/games/definitions/outwit/meta.ts
@@ -2,12 +2,13 @@ import { OutwitDefinition } from "./_types";
 
 const outwitMeta: OutwitDefinition["meta"] = {
   id: "outwit",
-  author: "(Uncredited)",
+  author: "unknown",
   name: "Outwit",
   tags: ["slide"],
   tagline: "It looks easy 'till you try it!",
   source: "https://boardgamegeek.com/boardgame/4696/outwit",
   code: "Pu",
+  demo: "Pu0Gri|D4d20,4io:ihnchgmblbhchcgaba1:b0Yg1bbb0Zk",
   slug: "outwit",
   added: "2020-12-13",
   addedBy: "Joel Fransson",

2f8e18bab762d996f5cefe9d05bef61533d8cab9
David <david@krawaller.se>
2021-03-21 10:12:02 +0100

    add gravatar package

--- modules/ui/src/components/ChallengeList/ChallengeList.tsx
+++ modules/ui/src/components/ChallengeList/ChallengeList.tsx
@@ -1,4 +1,5 @@
 import React, { FunctionComponent } from "react";
+import Gravatar from "react-gravatar";
 import { AlgolRemoteChallenge } from "../../../../remote/types/api/challenge";
 import { AlgolRemoteUser } from "../../../../remote/types/api/user";
 import css from "./ChallengeList.cssProxy";
@@ -12,6 +13,9 @@ export const ChallengeList: FunctionComponent<ChallengeListProps> = props => {
   const { challenges, user } = props;
   console.log("ChallengeList!", challenges, user);
   return (
-    <div className={css.challengeListContainer}>Stub for ChallengeList</div>
+    <div className={css.challengeListContainer}>
+      Stub for ChallengeList{" "}
+      <Gravatar md5="0bb93d13ba17249145c60bdf0f509bc7" size={80} />
+    </div>
   );
 };

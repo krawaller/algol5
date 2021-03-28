af8e009fe7f3151f3c932973b6a5615a9e4c6828
David <david@krawaller.se>
2021-03-28 11:40:21 +0200

    remote form foundation

--- modules/ui/src/components/NewRemoteSession/NewRemoteSession.tsx
+++ modules/ui/src/components/NewRemoteSession/NewRemoteSession.tsx
@@ -1,11 +1,13 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import {
   useMyCurrentGameChallenges,
   useForeignCurrentGameChallenges,
 } from "../../../../remote/utils/context";
+import { useGameAPI } from "../../contexts";
 import { AuthGuard } from "../AuthGuard";
 import { Box } from "../Box";
 import { ChallengeList } from "../ChallengeList";
+import { VariantSelector } from "../VariantSelector";
 import css from "./NewRemoteSession.cssProxy";
 
 export const NewRemoteSession = () => {
@@ -15,12 +17,23 @@ export const NewRemoteSession = () => {
 const Inner = () => {
   const myChallenges = useMyCurrentGameChallenges();
   const otherChallenges = useForeignCurrentGameChallenges();
-
+  const api = useGameAPI();
+  const filteredVariants = api.variants.filter(v => !v.hidden);
+  const [variant, setVariant] = useState(filteredVariants[0].code);
   return (
     <Fragment>
       <div className={css.newRemoteSessionTopInstruction}>
         Online play not yet fully implemented!
       </div>
+      <Box title="Issue challenge">
+        <div className={css.newRemoteSessionForm}>
+          <VariantSelector
+            variants={filteredVariants}
+            current={variant}
+            onSelect={setVariant}
+          />
+        </div>
+      </Box>
       <Box title="My challenges">
         <ChallengeList challenges={myChallenges} />
       </Box>

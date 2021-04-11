b54b867f3980e4e9bfd08a7473b8748d48485897
David <david@krawaller.se>
2021-04-11 17:51:58 +0200

    fix gamepage story

--- modules/ui/src/components/GamePage/GamePage.stories.tsx
+++ modules/ui/src/components/GamePage/GamePage.stories.tsx
@@ -6,12 +6,17 @@ import { GamePage } from ".";
 import { GameId, list } from "../../../../games/dist/list";
 
 import allGamePayloads from "../../../../payloads/dist/games";
+import { GamePayloadContext } from "../../contexts";
 
 storiesOf("GamePage", module).add(
   "Used for individual game pages in the app",
   () => {
     const gameId = select("Game", list, list[0]) as GameId;
     const payload = allGamePayloads[gameId];
-    return <GamePage key={gameId} gamePayload={payload} />;
+    return (
+      <GamePayloadContext.Provider value={payload}>
+        <GamePage key={gameId} />
+      </GamePayloadContext.Provider>
+    );
   }
 );

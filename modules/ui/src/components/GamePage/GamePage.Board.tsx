6d5d9b9b899b78609f139e2e75b0512381773d59
David <david@krawaller.se>
2021-07-09 13:24:43 +0200

    dont show active board when waiting for remote

--- modules/ui/src/components/GamePage/GamePage.Board.tsx
+++ modules/ui/src/components/GamePage/GamePage.Board.tsx
@@ -1,22 +1,23 @@
 import React from "react";
 import { BattleMode } from "../../contexts";
 import { Board } from "../Board";
-import {
-  AlgolBattle,
-  AlgolBattleUI,
-  AlgolGameGraphics,
-} from "../../../../types";
+import { AlgolBattleUI, AlgolGameGraphics } from "../../../../types";
+import { BattleHookState } from "./GamePage.useBattleActionsAndState";
+import { isWaitingForRemote } from "../../../../common";
 
 type GamePageBoardProps = {
   ui: AlgolBattleUI;
   mode: BattleMode;
-  battle?: AlgolBattle | null;
+  battleState?: BattleHookState | null;
   graphics: AlgolGameGraphics;
   mark: (pos: string) => void;
 };
 
 export const GamePageBoard = (props: GamePageBoardProps) => {
-  const { ui, mode, battle, graphics, mark } = props;
+  const { ui, mode, graphics, mark, battleState } = props;
+  const { battle, session } = battleState ?? ({} as BattleHookState);
+  const active =
+    mode === "playing" && !battle!.gameEndedBy && !isWaitingForRemote(session);
   return (
     <Board
       callback={mark}
@@ -25,7 +26,7 @@ export const GamePageBoard = (props: GamePageBoardProps) => {
       marks={ui.board.marks}
       potentialMarks={ui.board.potentialMarks}
       anim={ui.board.anim}
-      active={mode === "playing" && !battle!.gameEndedBy}
+      active={active}
       name={mode !== "gamelobby" ? battle!.variant.board : "basic"}
     />
   );

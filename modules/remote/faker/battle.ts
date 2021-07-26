bc14373b30dea41ff4b97ea89df52a7b8a8fadf3
David <david@krawaller.se>
2021-07-26 10:02:25 +0200

    make randy act if his turn when loading

--- modules/remote/faker/battle.ts
+++ modules/remote/faker/battle.ts
@@ -1,5 +1,6 @@
 import { randomEndTurn } from "../../battle/expose/randomEndTurn";
-import { updateSession } from "../../common";
+import { isWaitingForRemote, updateSession } from "../../common";
+import { AlgolBattle, AlgolSession } from "../../types";
 import { AlgolRemoteBattleAPI } from "../types/api/battle";
 import { currentGame } from "./atoms";
 import { makeFakeBattle } from "./makeFakeBattle";
@@ -10,6 +11,26 @@ const subs: Record<
   Parameters<AlgolRemoteBattleAPI["subscribe"]>["0"]["listener"]
 > = {};
 
+const fakeContainer = (id: string) => {
+  const api = currentGame.getValue()!;
+  const me = Math.random() < 0.5 ? 1 : 2;
+  const battle = makeFakeBattle(api);
+  const session = makeFakeSession({
+    api,
+    battle,
+    id,
+    me,
+  });
+  return { battle, session };
+};
+
+const fakeMove = (battle: AlgolBattle, session: AlgolSession) => {
+  const api = currentGame.getValue()!;
+  const updatedBattle = randomEndTurn(api, battle);
+  const updatedSession = updateSession(updatedBattle, session, api.iconMap);
+  return { battle: updatedBattle, session: updatedSession };
+};
+
 export const fakerBattleAPI: AlgolRemoteBattleAPI = {
   subscribe: opts => {
     subs[opts.sessionId] = opts.listener;
@@ -19,16 +40,14 @@ export const fakerBattleAPI: AlgolRemoteBattleAPI = {
   },
   load: (id: string) =>
     new Promise(resolve => {
-      const api = currentGame.getValue()!;
+      const container = fakeContainer(id);
       setTimeout(() => {
-        const battle = makeFakeBattle(api);
-        const session = makeFakeSession({
-          api,
-          battle,
-          id,
-          me: Math.random() < 0.5 ? 1 : 2,
-        });
-        resolve({ battle, session });
+        resolve(container);
+        if (isWaitingForRemote(container.session)) {
+          setTimeout(() => {
+            subs[id]?.(fakeMove(container.battle, container.session));
+          }, 2000 + 5000 * Math.random());
+        }
       }, 500 + 2000 * Math.random());
     }),
   endTurn: opts =>
@@ -47,16 +66,7 @@ export const fakerBattleAPI: AlgolRemoteBattleAPI = {
           session: updatedSession,
         });
         setTimeout(() => {
-          const randomBattle = randomEndTurn(api, updatedBattle);
-          const randomSession = updateSession(
-            randomBattle,
-            updatedSession,
-            api.iconMap
-          );
-          subs[opts.session.id]?.({
-            battle: randomBattle,
-            session: randomSession,
-          });
+          subs[opts.session.id]?.(fakeMove(updatedBattle, updatedSession));
         }, 2000 + 5000 * Math.random());
       }, 500 + 2000 * Math.random());
     }),
